07f0f9ace8ee454a4785241cec4b94e4e024c43b
Olivier Cado <cado@nevrax.com>
2000-09-19 11:14:03 +0200

    Updated comments

--- nel/include/nel/net/message.h
+++ nel/include/nel/net/message.h
@@ -1,14 +1,24 @@
 /* message.h
  *
- * Copyright (C) 2000 Nevrax. All rights reserved.
+ * Copyright, 2000 Nevrax Ltd.
  *
- * The redistribution, use and modification in source or binary forms of
- * this software is subject to the conditions set forth in the copyright
- * document ("Copyright") included with this distribution.
+ * This file is part of NEVRAX NEL.
+ * NEVRAX NEL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ * NEVRAX NEL is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with NEVRAX NEL; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA.
  */
 
 /*
- * $Id: message.h,v 1.2 2000/09/18 17:13:15 cado Exp $
+ * $Id: message.h,v 1.3 2000/09/19 09:14:03 cado Exp $
  *
  * Interface of CMessage
  */
--- nel/include/nel/net/socket.h
+++ nel/include/nel/net/socket.h
@@ -1,14 +1,24 @@
 /* socket.h
  *
- * Copyright (C) 2000 Nevrax. All rights reserved.
+ * Copyright, 2000 Nevrax Ltd.
  *
- * The redistribution, use and modification in source or binary forms of
- * this software is subject to the conditions set forth in the copyright
- * document ("Copyright") included with this distribution.
+ * This file is part of NEVRAX NEL.
+ * NEVRAX NEL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ * NEVRAX NEL is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with NEVRAX NEL; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA.
  */
 
 /*
- * $Id: socket.h,v 1.3 2000/09/18 17:13:15 cado Exp $
+ * $Id: socket.h,v 1.4 2000/09/19 09:14:03 cado Exp $
  *
  * Interface for CSocket
  */
@@ -94,7 +104,7 @@ public:
 	CSocket( SOCKET sock, const CInetAddress& remoteaddr ) throw (ESocket);
 
 	// Destructor
-	//~CSocket();
+	~CSocket();
 	
 	/// Sets/unsets TCP_NODELAY
 	void	setNoDelay( bool value ) throw (ESocket);
@@ -111,7 +121,7 @@ public:
 	/// Checks if there are some data to receive
 	bool	dataAvailable() throw (ESocket);
 
-	/// Receives data (returns false if !dataAvailable()). The capacity of the message must be large enough.
+	/// Receives data (returns false if !dataAvailable()).
 	bool	receive( CMessage& message ) throw (ESocket);
 
 	/// Returns the address of the remote host

a038e6581552d56b05a1d5f5247a83eef753ec43
Cedric Valignat <valignat@nevrax.com>
2000-11-23 12:48:50 +0100

    Fix some files name problems

--- nel/src/misc/config_file/do.bat
+++ nel/src/misc/config_file/do.bat
@@ -1,8 +1,8 @@
 @echo off
 echo Generating config_file.lex.cpp
-flex -f -8 -Pcf -Saceflex.skl -oconfig_file.lex.cpp config_file.lex
+flex -f -8 -Pcf -Scfflex.skl -oconfig_file.lex.cpp config_file.lex
 
 echo Generating config_file.yacc.cpp
-set BISON_SIMPLE=acebison.simple
+set BISON_SIMPLE=cfbison.simple
 bison -d -p cf -o config_file.yacc.cpp config_file.yacc
 pause

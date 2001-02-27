c9ad8606e368a15145acb1e349eb4efdbff8ddb0
Sameh Chafik <chafik@nevrax.com>
2001-02-27 15:12:29 +0100

    add IdFuzzyControlerScript.

--- nel/src/ai/static_def_init.cpp
+++ nel/src/ai/static_def_init.cpp
@@ -362,6 +362,10 @@
 		NLAIC::CTypeOfOperator::opAdd |
 		NLAIC::CTypeOfOperator::opEq);
 
+	const NLAIC::CIdentType CFuzzyControlerScript::IdFuzzyControlerScript("FuzzyControlerScript", NLAIC::CSelfClassCFactory( CFuzzyControlerScript() ),
+		NLAIC::CTypeOfObject(NLAIC::CTypeOfObject::tObject),
+		NLAIC::CTypeOfOperator(NLAIC::CTypeOfOperator::opNone));
+
 	const NLAIC::CIdentType CFuzzyRuleSet::idFuzzyRuleSet("FuzzyRuleSet", NLAIC::CSelfClassCFactory( CFuzzyRuleSet() ),
 		NLAIC::CTypeOfObject(NLAIC::CTypeOfObject::tObject),
 		NLAIC::CTypeOfOperator(NLAIC::CTypeOfOperator::opNone));

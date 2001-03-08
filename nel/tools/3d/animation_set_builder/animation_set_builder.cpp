226f387464d95a9a9fe0b0da46aefa762768a10a
Cyril Corvazier <corvazier@nevrax.com>
2001-03-08 15:11:57 +0100

    ADDED: new tools used to merge animations in an animation set

---
+++ nel/tools/3d/animation_set_builder/animation_set_builder.cpp
@@ -0,0 +1,109 @@
+// animation_set_builder.cpp : Defines the entry point for the console application.
+//
+
+#include <nel/misc/file.h>
+#include <nel/3d/animation_set.h>
+
+using namespace NLMISC;
+using namespace NL3D;
+
+int main(int argc, char* argv[])
+{
+	// Check num of args
+	if (argc!=3)
+	{
+		// Output an help
+		printf  (
+					"animation_set_builder [animation_set.animset] [animation.anim]\n"
+					"\tThis command line add the animation.anim animation into the animation_set.animset animation set\n"
+				);
+	}
+	else
+	{
+		try
+		{
+			// Tha animation set
+			CAnimationSet animSet;
+
+			// Try to open the animation set file
+			CIFile inFile;
+			if (inFile.open (argv[1]))
+			{
+				// Ok, read it
+				animSet.serial (inFile);
+
+				// Close it
+				inFile.close ();
+			}
+			else
+			{
+				// Error message
+				printf ("Info: can't open %s for reading. Create the file.\n", argv[1]);
+				
+				// Error code
+				return -1;
+			}
+
+			// Animation
+			CAnimation anim;
+
+			// Read the input animation
+			if (inFile.open (argv[2]))
+			{
+				// Ok, read it
+				anim.serial (inFile);
+
+				// Close it
+				inFile.close ();
+
+				// *** Build the animation set
+
+				// Add an animation
+				uint id=animSet.addAnimation (anim.getName().c_str());
+
+				// Copy the animation
+				*animSet.getAnimation (id)=anim;
+
+				// Build the animation set
+				animSet.build ();
+
+				// *** Save the animation Set
+
+				// Open a output file
+				COFile outFile;
+				if (outFile.open (argv[1]))
+				{
+					// Write it
+					animSet.serial (outFile);
+				}
+				else
+				{
+					// Error message
+					fprintf (stderr, "Error: can't open %s for writing.\n", argv[1]);
+					
+					// Error code
+					return -1;
+				}
+
+				// That's it.
+			}
+			else
+			{
+				// Error message
+				fprintf (stderr, "Error: can't open %s for reading.\n", argv[2]);
+				
+				// Error code
+				return -1;
+			}
+		}
+		catch (Exception& e)
+		{
+			// Error message
+			fprintf (stderr, "Error: %s\n", e.what());
+			
+			// Error code
+			return -1;
+		}
+	}
+	return 0;
+}

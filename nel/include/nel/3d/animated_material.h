f5aa9c7da216b8815cfe74f771161b0486eaff81
Cyril Corvazier <corvazier@nevrax.com>
2001-03-13 18:07:53 +0100

    ADDED: class to manage materal animations

---
+++ nel/include/nel/3d/animated_material.h
@@ -0,0 +1,64 @@
+/** \file animated_material.h
+ * <File description>
+ *
+ * $Id: animated_material.h,v 1.1 2001/03/13 17:07:53 corvazier Exp $
+ */
+
+/* Copyright, 2001 Nevrax Ltd.
+ *
+ * This file is part of NEVRAX NEL.
+ * NEVRAX NEL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+
+ * NEVRAX NEL is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with NEVRAX NEL; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA.
+ */
+
+#ifndef NL_ANIMATED_MATERIAL_H
+#define NL_ANIMATED_MATERIAL_H
+
+#include "nel/misc/types_nl.h"
+
+
+namespace NL3D 
+{
+
+
+/**
+ * <Class description>
+ * \author Cyril 'Hulud' Corvazier
+ * \author Nevrax France
+ * \date 2001
+ */
+class CAnimatedMaterial
+{
+public:
+
+	/// Constructor
+	CAnimatedMaterial();
+
+	/// \name Get some track name
+
+	/// Return the name of the pos track
+	static const char *getAmbientValueName() {return "ambient";}
+	static const char *getDiffuseValueName() {return "diffuse";}
+	static const char *getSpecularValueName() {return "specular";}
+	static const char *getOpacityValueName() {return "opacity";}
+};
+
+
+} // NL3D
+
+
+#endif // NL_ANIMATED_MATERIAL_H
+
+/* End of animated_material.h */

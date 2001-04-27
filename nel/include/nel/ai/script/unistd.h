d47316448da45b25688f0b43fd32a44760ddb594
cvs-fast-export <cvs-fast-export@ryzom.dev>
2001-04-27 14:15:50 +0200

    Synthetic commit for incomplete tag net_0_1

--- nel/include/nel/ai/script/unistd.h
+++
@@ -1,9 +0,0 @@
-#ifndef NL_UNISTD_H
-#define NL_UNISTD_H
-
-
-#include <iostream>
-#include <strstream>
-using namespace std;
-
-#endif
--- nel/src/misc/config_file/do.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Generating config_file.lex.cpp
-flex -f -8 -Pcf -Scfflex.skl -oconfig_file.lex.cpp config_file.lex
-
-echo Generating config_file.yacc.cpp
-set BISON_SIMPLE=cfbison.simple
-bison -d -p cf -o config_file.yacc.cpp config_file.yacc
-pause

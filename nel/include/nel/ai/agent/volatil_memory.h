b841615f2ce7d85989470357ddc121c758456943
Sameh Chafik <chafik@nevrax.com>
2001-05-31 15:30:14 +0200

    add CVolatilMemmory

--- nel/include/nel/ai/agent/volatil_memory.h
+++ nel/include/nel/ai/agent/volatil_memory.h
@@ -1,6 +1,6 @@
 /** \file volatil_memory.h
  *
- * $Id: volatil_memory.h,v 1.1 2001/05/29 15:19:20 chafik Exp $
+ * $Id: volatil_memory.h,v 1.2 2001/05/31 13:30:14 chafik Exp $
  */
 
 /* Copyright, 2000 Nevrax Ltd.
@@ -23,16 +23,106 @@
  */
 #include "nel/ai/agent/agent.h"
 #include "nel/ai/agent/agent_object.h"
+#include "nel/ai/agent/agent_timer.h"
 
 namespace NLAIAGENT
-{		
-	class CVolatilMemmory: public IAgent
+{	
+	class CHashTimerManager: public IConnectIA
 	{
 	public:
-		static const NLAIC::CIdentType IdVolatilMemmory;
+		static const NLAIC::CIdentType *IdHashTimerManager;
+
+	public:
+		static sint MaxSubscribByTimer;
+		static uint Time;
 
 	private:
+		std::list<CAgentWatchTimer *> TimerAccount;		
+
+	public:
+		CHashTimerManager() {}
+		CHashTimerManager(const CHashTimerManager &);
+		~CHashTimerManager() {}
+
+
+		CAgentWatchTimer *getTimer();
+		
+
+		virtual void getDebugString(std::string &t) const
+		{
+			t += "HashTimerManager";
+		}
+
+		virtual const NLAIC::CIdentType &getType() const
+		{
+			return *IdHashTimerManager;
+		}
+
+		virtual void save(NLMISC::IStream &os)
+		{
+		}
+		virtual void load(NLMISC::IStream &is)
+		{
+		}
+
+		virtual const NLAIC::IBasicType *newInstance() const
+		{			
+			NLAIC::IBasicType *x = new CHashTimerManager();		
+			return x;
+		}
+
+		virtual const NLAIC::IBasicType *clone() const
+		{			
+			NLAIC::IBasicType *x = new CHashTimerManager(*this);			
+			return x;
+		}
+
+		virtual bool isEqual(const IBasicObjectIA &a) const
+		{
+			return this == &a;
+		}
+
+		virtual void onKill(IConnectIA *)
+		{
+		}
+
+		virtual IObjectIA::CProcessResult sendMessage(IMessageBase *msg)
+		{
+			return IObjectIA::CProcessResult();
+		}
+
+		virtual const CProcessResult &getState() const
+		{
+			return IObjectIA::ProcessRun;
+		}
+
+		virtual void setState(TProcessStatement s, IObjectIA *result) 
+		{
+		}
+		virtual const CProcessResult &run()
+		{
+			return IObjectIA::ProcessRun;
+		}
+
+	public:
+		static void initClass();
+		static void releaseClass();
+
+	};
+	class CVolatilMemmory: public IConnectIA
+	{
+	public:		
+		static CHashTimerManager *Timer;
+
+	public:
+		static const NLAIC::CIdentType *IdVolatilMemmory;
+
+	private:
+		uint _Time;
 		IBaseGroupType *_List;
+		std::list<IObjectIA *> _Connecter; 
+		const CPaireType *_PairToDelete;
+
 
 	public:
 		CVolatilMemmory(IBaseGroupType *l = new CGroupType);
@@ -40,19 +130,40 @@ namespace NLAIAGENT
 		virtual ~CVolatilMemmory();		
 
 
-		IBaseGroupType *getList() const
+		CConstIteratorContener getConstIterator() const
 		{
-			return _List;
+			return _List->getConstIterator();
 		}
+
+		void pushFront(uint time,IObjectIA *);
+		void pushBack(uint time, IObjectIA *);
+
+		void popFront();
+		void popBack();
+		IObjectIA *Front() const;
+		IObjectIA *Back() const;
+		void erase(CConstIteratorContener &);
+
+		void addAccount(IObjectIA *);
+		void releaseAccount(IObjectIA *);
+		
+		void runMessage(CAgentWatchTimer *);
+
+		void init();
+
 
 		/// \name IBasicInterface method.
 		//@{
-		virtual void save(NLMISC::IStream &os);		
-		virtual void load(NLMISC::IStream &is);
+		virtual void save(NLMISC::IStream &os)
+		{
+		}
+		virtual void load(NLMISC::IStream &is)
+		{
+		}
 		
 		virtual const NLAIC::CIdentType &getType() const
 		{
-			return IdVolatilMemmory;
+			return *IdVolatilMemmory;
 		}
 
 		virtual const NLAIC::IBasicType *newInstance() const
@@ -65,9 +176,27 @@ namespace NLAIAGENT
 		{			
 			NLAIC::IBasicType *x = new CVolatilMemmory(*this);			
 			return x;
-		}		
+		}
 
-		virtual void getDebugString(std::string &t) const;		
+		virtual void getDebugString(std::string &t) const;
+
+		virtual bool isEqual(const IBasicObjectIA &a) const
+		{
+			return this == &a;
+		}
+
+		virtual void onKill(IConnectIA *);	
+
+		virtual IObjectIA::CProcessResult sendMessage(IMessageBase *msg);
+
+		virtual const CProcessResult &getState() const
+		{
+			return IObjectIA::ProcessRun;
+		}
+		virtual void setState(TProcessStatement s, IObjectIA *result) 
+		{
+		}
+
 		//@}
 
 		/// \name IObjectIA method.
@@ -77,5 +206,12 @@ namespace NLAIAGENT
 			return IObjectIA::ProcessRun;
 		}
 		//@}
+
+	private:
+		void searchBest();
+		void testIfBest(const CPaireType *);
+	public:
+		static void initClass();
+		static void releaseClass();
 	};
 }

bd3533672441f396566c1dfbdf0ca56411e3e441
Olivier Cado <cado@nevrax.com>
2001-06-18 11:02:16 +0200

    Initial import of message recorder

---
+++ nel/src/net/dummy_tcp_sock.cpp
@@ -0,0 +1,66 @@
+/** \file dummy_tcp_sock.cpp
+ * <File description>
+ *
+ * $Id: dummy_tcp_sock.cpp,v 1.1 2001/06/18 09:02:01 cado Exp $
+ */
+
+/* Copyright, 2001 Nevrax Ltd.
+ *
+ * This file is part of NEVRAX NEL.
+ * NEVRAX NEL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+
+ * NEVRAX NEL is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with NEVRAX NEL; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA.
+ */
+
+#include "nel/net/dummy_tcp_sock.h"
+#include "nel/misc/debug.h"
+
+using namespace NLMISC;
+
+
+namespace NLNET {
+
+
+/*
+ * Set only the remote address
+ */
+void CDummyTcpSock::connect( const CInetAddress& addr )
+{
+	_RemoteAddr = addr;
+	_Sock = 100;
+
+	_BytesReceived = 0;
+	_BytesSent = 0;
+
+	CSynchronized<bool>::CAccessor sync( &_SyncConnected );
+	sync.value() = true;
+
+	nldebug( "L0: Socket connected to %s", addr.asString().c_str() );
+}
+
+
+/*
+ *Dummy disconnection
+ */
+void CDummyTcpSock::disconnect()
+{
+	nldebug( "L0: Socket disconnecting from %s...", _RemoteAddr.asString().c_str() );
+
+	CSynchronized<bool>::CAccessor sync( &_SyncConnected );
+	sync.value() = false;
+}
+
+
+
+} // NLNET

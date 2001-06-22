681970b3ba46d67a20b6893217d775c9cc320482
Cyril Corvazier <corvazier@nevrax.com>
2001-06-22 16:59:20 +0200

    #nothing changed

--- nel/tools/3d/object_viewer/resource.h
+++ nel/tools/3d/object_viewer/resource.h
@@ -221,7 +221,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        1048
 #define _APS_NEXT_COMMAND_VALUE         32810
-#define _APS_NEXT_CONTROL_VALUE         1125
+#define _APS_NEXT_CONTROL_VALUE         1121
 #define _APS_NEXT_SYMED_VALUE           1000
 #endif
 #endif

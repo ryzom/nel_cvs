2a48ab960fc7aee1ca7991bf1b1193b09da382cb
Nicolas Vizerie <vizerie@nevrax.com>
2001-06-25 15:00:37 +0200

    #ADDED: added the copyright that was missing,

--- nel/tools/3d/object_viewer/ps_mover_dlg.cpp
+++ nel/tools/3d/object_viewer/ps_mover_dlg.cpp
@@ -1,11 +1,34 @@
-// ps_mover_dlg.cpp : implementation file
-//
+/** \file ps_mover_dlg.cpp
+ * this dialog display coordinate of an instance of a located in a particle system 
+ * $Id: ps_mover_dlg.cpp,v 1.4 2001/06/25 13:00:37 vizerie Exp $
+ */
+
+/* Copyright, 2000 Nevrax Ltd.
+ *
+ * This file is part of NEVRAX NEL.
+ * NEVRAX NEL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+
+ * NEVRAX NEL is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with NEVRAX NEL; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA.
+ */
 
 #include "std_afx.h"
 #include "object_viewer.h"
 #include "ps_mover_dlg.h"
 #include "particle_tree_ctrl.h"
 #include "3d/ps_located.h"
+
+#include "editable_range.h"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -17,8 +40,11 @@ static char THIS_FILE[] = __FILE__;
 // CPSMoverDlg dialog
 
 
-CPSMoverDlg::CPSMoverDlg(CParticleTreeCtrl *parent, HTREEITEM editedItem)   // standard constructor
+CPSMoverDlg::CPSMoverDlg(CParticleTreeCtrl *parent, NL3D::CEvent3dMouseListener *ml,  HTREEITEM editedItem)   // standard constructor
 	: CDialog(CPSMoverDlg::IDD, parent)
+	  , _MouseListener(ml)
+	  ,	_Scale(NULL), _XScale(NULL), _YScale(NULL), _ZScale(NULL)
+	  , _ScaleText(NULL), _XScaleText(NULL), _YScaleText(NULL), _ZScaleText(NULL)
 {
 
 	_TreeCtrl = parent ;
@@ -35,6 +61,21 @@ CPSMoverDlg::CPSMoverDlg(CParticleTreeCtrl *parent, HTREEITEM editedItem)   // s
 	//{{AFX_DATA_INIT(CPSMoverDlg)
 	//}}AFX_DATA_INIT
 
+
+}
+
+CPSMoverDlg::~CPSMoverDlg()
+{
+	cleanScaleCtrl() ;
+}
+
+void CPSMoverDlg::updateListener(void)
+{
+	NLMISC::CVector &pos = _EditedNode->Loc->getPos()[_EditedNode->LocatedInstanceIndex] ;
+	NLMISC::CMatrix m ;
+	m = _MouseListener->getModelMatrix() ;
+	m.setPos(pos) ;
+	_MouseListener->setModelMatrix(m) ;
 
 }
 
@@ -82,6 +123,7 @@ void CPSMoverDlg::OnUpdateXpos()
 	if (::sscanf(m_X, "%f", &x) == 1)
 	{
 		pos.x = x ;
+		updateListener() ;
 	}
 	else
 	{
@@ -98,6 +140,7 @@ void CPSMoverDlg::OnUpdateYpos()
 	if (::sscanf(m_Y, "%f", &y) == 1)
 	{
 		pos.y = y ;
+		updateListener() ;
 	}
 	else
 	{
@@ -114,6 +157,7 @@ void CPSMoverDlg::OnUpdateZpos()
 	if (::sscanf(m_Z, "%f", &z) == 1)
 	{
 		pos.z = z ;
+		updateListener() ;		
 	}
 	else
 	{
@@ -134,15 +178,115 @@ BOOL CPSMoverDlg::OnInitDialog()
 	{
 		if (dynamic_cast<NL3D::IPSMover *>(_EditedNode->Loc->getBoundObject(k)))
 		{
-			m_SubComponentCtrl.AddString(_EditedNode->Loc->getBoundObject(k)->getName().c_str()) ;
-			m_SubComponentCtrl.SetItemData(nbCandidates, (DWORD) _EditedNode->Loc->getBoundObject(k)) ;
+			uint insertedLine = m_SubComponentCtrl.AddString(_EditedNode->Loc->getBoundObject(k)->getName().c_str()) ;
+			m_SubComponentCtrl.SetItemData(insertedLine, (DWORD) _EditedNode->Loc->getBoundObject(k)) ;
 			++nbCandidates ;			
 		}
 	}
-	m_SubComponentCtrl.SetSel(0) ;	
+
+	if (nbCandidates > 0)
+	{
+		m_SubComponentCtrl.SetCurSel(0) ;	
+		_EditedNode->Loc->getOwner()->setCurrentEditedElement(_EditedNode->Loc, _EditedNode->LocatedInstanceIndex, (NL3D::CPSLocatedBindable *) m_SubComponentCtrl.GetItemData(0)) ;
+	}
 	
 	return TRUE;  // return TRUE unless you set the focus to a control
 	              // EXCEPTION: OCX Property Pages should return FALSE
+}
+
+void CPSMoverDlg::cleanScaleCtrl(void)
+{
+	#define REMOVE_SCALE_WINDOW(w) if (w) { w->DestroyWindow() ; delete w ; w = NULL ; }
+									
+	REMOVE_SCALE_WINDOW(_Scale) ;
+	REMOVE_SCALE_WINDOW(_XScale) ;
+	REMOVE_SCALE_WINDOW(_YScale) ;
+	REMOVE_SCALE_WINDOW(_ZScale) ;
+
+	REMOVE_SCALE_WINDOW(_ScaleText) ;
+	REMOVE_SCALE_WINDOW(_XScaleText) ;
+	REMOVE_SCALE_WINDOW(_YScaleText) ;
+	REMOVE_SCALE_WINDOW(_ZScaleText) ;
+}
+
+void CPSMoverDlg::createScaleControls(void)
+{
+	cleanScaleCtrl() ;
+
+	if (m_SubComponentCtrl.GetCurSel() == -1) return ;
+	
+	NL3D::CPSLocatedBindable *lb = (NL3D::CPSLocatedBindable *) m_SubComponentCtrl.GetItemData(m_SubComponentCtrl.GetCurSel()) ;
+	NL3D::IPSMover *m = dynamic_cast<NL3D::IPSMover *>(lb) ;
+
+	const sint xPos = 10 ;
+	sint yPos = 330 ;
+	
+
+	if (m->supportUniformScaling() && ! m->supportNonUniformScaling() )
+	{
+		_Scale = new CEditableRangeFloat("UNIFORM SCALE", 0.f, 4.f)  ;
+		_UniformScaleWrapper.M = m ;
+		_UniformScaleWrapper.Index = _EditedNode->LocatedInstanceIndex ;
+
+		_Scale->setWrapper(&_UniformScaleWrapper) ;
+		_Scale->init(100 + xPos, yPos, this) ;
+
+		_ScaleText = new CStatic ;
+		_ScaleText ->Create("Scale : ", SS_LEFT, CRect(xPos, yPos + 10, xPos + 60, yPos + 32), this) ;
+		_ScaleText ->ShowWindow(SW_SHOW) ;
+	}
+	else if (m->supportNonUniformScaling())
+	{
+		RECT r ;
+
+		// dialog for edition of x scale
+		_XScale = new CEditableRangeFloat("X SCALE", 0.f, 4.f)  ;
+		_XScaleWrapper.M = m ;
+		_XScaleWrapper.Index = _EditedNode->LocatedInstanceIndex ;
+		_XScale->setWrapper(&_XScaleWrapper) ;
+		_XScale->init(xPos + 70, yPos, this) ;
+		_XScale->GetClientRect(&r) ;
+
+		_XScaleText = new CStatic ;
+		_XScaleText ->Create("x Scale : ", SS_LEFT, CRect(xPos, yPos + 10, xPos + 60, yPos + 32), this) ;
+		_XScaleText ->ShowWindow(SW_SHOW) ;
+
+		yPos += r.bottom ;
+
+		// dialog for edition of y scale
+		_YScale = new CEditableRangeFloat("Y SCALE", 0.f, 4.f)  ;
+		_YScaleWrapper.M = m ;
+		_YScaleWrapper.Index = _EditedNode->LocatedInstanceIndex ;
+		_YScale->setWrapper(&_YScaleWrapper) ;
+		_YScale->init(xPos + 70, yPos, this) ;
+		_YScale->GetClientRect(&r) ;
+		
+
+		_YScaleText = new CStatic ;
+		_YScaleText ->Create("y Scale : ", SS_LEFT, CRect(xPos, yPos + 10, xPos + 60, yPos + 32), this) ;
+		_YScaleText ->ShowWindow(SW_SHOW) ;
+
+		yPos += r.bottom ;
+
+		// dialog for edition of x scale
+		_ZScale = new CEditableRangeFloat("Z SCALE", 0.f, 4.f)  ;
+		_ZScaleWrapper.M = m ;
+		_ZScaleWrapper.Index = _EditedNode->LocatedInstanceIndex ;
+		_ZScale->setWrapper(&_ZScaleWrapper) ;
+		_ZScale->init(xPos + 70, yPos, this) ;
+		_ZScale->GetClientRect(&r) ;
+		
+		_ZScaleText = new CStatic ;
+		_ZScaleText ->Create("z Scale : ", SS_LEFT, CRect(xPos, yPos + 10, xPos + 60, yPos + 32), this) ;
+		_ZScaleText ->ShowWindow(SW_SHOW) ;
+
+
+	}
+
+
+
+
+	
 }
 
 void CPSMoverDlg::OnSelchangeSubComponent() 
@@ -154,6 +298,8 @@ void CPSMoverDlg::OnSelchangeSubComponent()
 	_EditedNode->LocMover = dynamic_cast<NL3D::IPSMover *>(lb) ;
 	_EditedNode->Loc->getOwner()->setCurrentEditedElement(_EditedNode->Loc, _EditedNode->LocatedInstanceIndex, lb) ;
 	
+	createScaleControls() ;
+
 	UpdateData(FALSE) ;
 }
 
@@ -162,4 +308,5 @@ void CPSMoverDlg::init(CWnd *parent)
 
 	Create(CPSMoverDlg::IDD, parent) ;
 	ShowWindow(SW_SHOW) ;
+	createScaleControls() ;
 }
--- nel/tools/3d/object_viewer/ps_mover_dlg.h
+++ nel/tools/3d/object_viewer/ps_mover_dlg.h
@@ -1,11 +1,45 @@
+/** \file ps_mover_dlg.h
+ * this dialog display coordinate of an instance of a located in a particle system 
+ * $Id: ps_mover_dlg.h,v 1.3 2001/06/25 13:00:37 vizerie Exp $
+ */
+
+/* Copyright, 2000 Nevrax Ltd.
+ *
+ * This file is part of NEVRAX NEL.
+ * NEVRAX NEL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+
+ * NEVRAX NEL is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with NEVRAX NEL; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA.
+ */
+
 #if !defined(AFX_PS_MOVER_DLG_H__C1C4348E_3384_4557_B99E_CBE4E5492C0C__INCLUDED_)
 #define AFX_PS_MOVER_DLG_H__C1C4348E_3384_4557_B99E_CBE4E5492C0C__INCLUDED_
 
 #if _MSC_VER > 1000
 #pragma once
-#endif // _MSC_VER > 1000
-// ps_mover_dlg.h : header file
-//
+#endif 
+
+
+namespace NL3D
+{
+	class CEvent3dMouseListener ;
+}
+
+#include "ps_wrapper.h"
+
+template <class T> class CEditableRangeT ;
+typedef CEditableRangeT<float> CEditableRangeFloat ;
+
 
 /////////////////////////////////////////////////////////////////////////////
 // CPSMoverDlg dialog
@@ -15,7 +49,10 @@ class CPSMoverDlg : public CDialog
 // Construction
 public:
 	// construct the object with a pointer to the tree ctrl, and to the item that is being edited
-	CPSMoverDlg(class CParticleTreeCtrl *parent, HTREEITEM editedNode);   // standard constructor
+	CPSMoverDlg(class CParticleTreeCtrl *parent, NL3D::CEvent3dMouseListener *ml,  HTREEITEM editedNode);   // standard constructor
+
+	// dtor
+	~CPSMoverDlg() ;
 
 // Dialog Data
 	//{{AFX_DATA(CPSMoverDlg)
@@ -42,12 +79,25 @@ public:
 
 	void init(CWnd *parent) ;
 
+	
 
 // Implementation
 protected:
 
 	CParticleTreeCtrl *_TreeCtrl ;
 	CParticleTreeCtrl::CNodeType *_EditedNode ;
+	NL3D::CEvent3dMouseListener *_MouseListener ;
+
+
+	CEditableRangeFloat *_Scale, *_XScale, *_YScale, *_ZScale ;
+
+	CStatic  *_ScaleText, *_XScaleText, *_YScaleText, *_ZScaleText ;
+
+	// this generate control for scaling
+	void createScaleControls(void) ;
+
+	// delete scale controls if presents
+	void cleanScaleCtrl(void) ;
 
 
 	// Generated message map functions
@@ -59,6 +109,60 @@ protected:
 	afx_msg void OnSelchangeSubComponent();
 	//}}AFX_MSG
 	DECLARE_MESSAGE_MAP()
+
+
+	// wrappers to scale objects
+	struct tagUniformScaleWrapper : public IPSWrapperFloat
+	{
+		uint32 Index ;
+		NL3D::IPSMover *M ;
+		float get(void) const { return M->getScale(Index).x ; }
+		void set(const float &v) { M->setScale(Index, v) ; }
+	} _UniformScaleWrapper ;
+
+	/// wrapper to scale the X coordinate
+	struct tagXScaleWrapper : public IPSWrapperFloat
+	{
+		uint32 Index ;
+		NL3D::IPSMover *M ;
+		float get(void) const { return M->getScale(Index).x ; }
+		void set(const float &s) 
+		{ 
+			NLMISC::CVector v = M->getScale(Index) ;
+			M->setScale(Index, NLMISC::CVector(s, v.y, v.z)) ; 
+		}
+	} _XScaleWrapper ;
+
+	/// wrapper to scale the Y coordinate
+	struct tagYScaleWrapper : public IPSWrapperFloat
+	{
+		uint32 Index ;
+		NL3D::IPSMover *M ;
+		float get(void) const { return M->getScale(Index).y ; }
+		void set(const float &s) 
+		{ 
+			NLMISC::CVector v = M->getScale(Index) ;
+			M->setScale(Index, NLMISC::CVector(v.x, s, v.z) ) ; 
+		}
+	} _YScaleWrapper ;
+
+	/// wrapper to scale the Z coordinate
+	struct tagZScaleWrapper : public IPSWrapperFloat
+	{
+		uint32 Index ;
+		NL3D::IPSMover *M ;
+		float get(void) const { return M->getScale(Index).z ; }
+		void set(const float &s) 
+		{ 
+			NLMISC::CVector v = M->getScale(Index) ;
+			M->setScale(Index, NLMISC::CVector(v.x, v.y, s) ) ; 
+		}
+	} _ZScaleWrapper ;
+	 
+
+	// update the mouse listener position when the user entered a value with the keyboard
+	void CPSMoverDlg::updateListener(void) ;
+
 };
 
 //{{AFX_INSERT_LOCATION}}

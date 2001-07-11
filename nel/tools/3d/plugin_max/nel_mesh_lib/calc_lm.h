7241584dfda933a1274edeecb5ac6d6202f3f954
Matthieu Besson <besson@nevrax.com>
2001-07-11 12:11:42 +0200

    ADDED: AppData management

---
+++ nel/tools/3d/plugin_max/nel_mesh_lib/calc_lm.h
@@ -0,0 +1,47 @@
+/** \file calc_lm.h
+ * LightMap Calculation settings
+ *
+ * $Id: calc_lm.h,v 1.1 2001/07/11 10:11:42 besson Exp $
+ */
+
+/* Copyright, 2000 Nevrax Ltd.
+ *
+ * This file is part of NEVRAX NEL.
+ * NEVRAX NEL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+
+ * NEVRAX NEL is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with NEVRAX NEL; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA.
+ */
+
+#ifndef NL_CALC_LM_H
+#define NL_CALC_LM_H
+
+// ***************************************************************************
+
+#define MAXLIGHTMAPSIZE		1024
+
+// ***************************************************************************
+
+#define NEL3D_APPDATA_LM			41654684
+#define NEL3D_APPDATA_LM_DYNAMIC	(NEL3D_APPDATA_LM)
+#define NEL3D_APPDATA_LM_GROUPNAME	(NEL3D_APPDATA_LM+1)
+#define NEL3D_APPDATA_LM_ANIMATED	(NEL3D_APPDATA_LM+2)
+
+// ***************************************************************************
+
+
+// ***************************************************************************
+
+#endif // NL_CALC_LM_H
+
+/* End of calc_lm.h */

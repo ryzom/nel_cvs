09d0eea94346fb4b3fdc4bfc2ccab7b2a34b8287
Nicolas Vizerie <vizerie@nevrax.com>
2001-07-12 17:54:12 +0200

    #ADDED: contains statics of CPSAttribMakerBinOp

---
+++ nel/src/3d/ps_attrib_maker_bin_op.cpp
@@ -0,0 +1,48 @@
+/** \file ps_attrib_maker_bin_op.cpp
+ * <File description>
+ *
+ * $Id: ps_attrib_maker_bin_op.cpp,v 1.1 2001/07/12 15:54:12 vizerie Exp $
+ */
+
+/* Copyright, 2001 Nevrax Ltd.
+ *
+ * This file is part of NEVRAX NEL.
+ * NEVRAX NEL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+
+ * NEVRAX NEL is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with NEVRAX NEL; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA.
+ */
+
+#include "ps_attrib_maker_bin_op.h"
+
+
+namespace NL3D
+{
+
+// instanciation of template static members
+
+float CPSAttribMakerBinOp<float>::_Buf1[PSBinOpBufSize] ;
+float CPSAttribMakerBinOp<float>::_Buf2[PSBinOpBufSize] ;
+uint32 CPSAttribMakerBinOp<uint32>::_Buf1[PSBinOpBufSize] ;
+uint32 CPSAttribMakerBinOp<uint32>::_Buf2[PSBinOpBufSize] ;
+sint32 CPSAttribMakerBinOp<sint32>::_Buf1[PSBinOpBufSize] ;
+sint32 CPSAttribMakerBinOp<sint32>::_Buf2[PSBinOpBufSize] ;
+NLMISC::CRGBA CPSAttribMakerBinOp<NLMISC::CRGBA>::_Buf1[PSBinOpBufSize] ;
+NLMISC::CRGBA CPSAttribMakerBinOp<NLMISC::CRGBA>::_Buf2[PSBinOpBufSize] ;
+CPlaneBasis CPSAttribMakerBinOp<CPlaneBasis>::_Buf1[PSBinOpBufSize] ;
+CPlaneBasis CPSAttribMakerBinOp<CPlaneBasis>::_Buf2[PSBinOpBufSize] ;
+
+
+
+
+} // NL3D

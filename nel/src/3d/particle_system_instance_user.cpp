77ca3f076316f56c3adba5c4d2dfa76c32bf4b3c
Nicolas Vizerie <vizerie@nevrax.com>
2001-07-25 12:23:38 +0200

    ADDED: user interface for particles systems

---
+++ nel/src/3d/particle_system_instance_user.cpp
@@ -0,0 +1,140 @@
+/** \file particle_system_instance_user.cpp
+ * <File description>
+ *
+ * $Id: particle_system_instance_user.cpp,v 1.1 2001/07/25 10:23:38 vizerie Exp $
+ */
+
+/* Copyright, 2000, 2001 Nevrax Ltd.
+ *
+ * This file is part of NEVRAX NEL.
+ * NEVRAX NEL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+
+ * NEVRAX NEL is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with NEVRAX NEL; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA.
+ */
+
+#include "3d/particle_system_instance_user.h"
+#include "3d/particle_system.h"
+
+
+namespace NL3D {
+
+CParticleSystemInstanceUser::CParticleSystemInstanceUser(CScene *scene, IModel *model) : CTransformUser(scene, model)
+{
+	CParticleSystemModel *psm = NLMISC::safe_cast<CParticleSystemModel *>(_Transform) ;
+	psm->registerDtorObserver(this) ;
+}
+
+
+CParticleSystemInstanceUser::~CParticleSystemInstanceUser()
+{
+	// if the model hasn't been detroyed yet ...
+	if (_Transform)
+	{
+		CParticleSystemModel *psm = NLMISC::safe_cast<CParticleSystemModel *>(_Transform) ;
+		psm->removeDtorObserver(this) ;	
+	}
+}
+
+bool		CParticleSystemInstanceUser::isSystemPresent(void) const
+{
+	if (!_Transform) return false ; // the system is not even valid
+	CParticleSystemModel *psm = NLMISC::safe_cast<CParticleSystemModel *>(_Transform) ;
+	return psm->getPS() != NULL ;
+}
+
+bool		CParticleSystemInstanceUser::getBBox(NLMISC::CAABBox &bbox)
+{
+	if (!_Transform) return false ;
+	CParticleSystemModel *psm = NLMISC::safe_cast<CParticleSystemModel *>(_Transform) ;
+	if (!psm->getPS()) return false ;
+	psm->getPS()->computeBBox(bbox) ;
+	return true ;
+}
+
+void		CParticleSystemInstanceUser::setUserParam(uint index, float value)
+{
+	nlassert(isSystemPresent())      ; // user : you forgot to check wether the system was present with isPresent() !!
+	nlassert(index < MaxPSUserParam) ; // invalid parameter index
+	CParticleSystemModel *psm = NLMISC::safe_cast<CParticleSystemModel *>(_Transform) ;
+	psm->getPS()->setUserParam(index, value) ;
+}
+
+float		CParticleSystemInstanceUser::getUserParam(uint index) const
+{
+	nlassert(isSystemPresent())      ; // user : you forgot to check wether the system was present with isPresent() !!
+	nlassert(index < MaxPSUserParam) ; // invalid parameter index
+	CParticleSystemModel *psm = NLMISC::safe_cast<CParticleSystemModel *>(_Transform) ;
+	return psm->getPS()->getUserParam(index) ;
+
+}
+
+bool		CParticleSystemInstanceUser::isValid(void) const
+{
+	return _Transform != NULL ;
+}
+
+void		CParticleSystemInstanceUser::registerPSObserver(IPSObserver *observer)
+{
+	nlassert(!isPSObserver(obesrver)) ;
+	_Observers.push_back(observer) ;
+}
+
+bool		CParticleSystemInstanceUser::isPSObserver(IPSObserver *observer)
+{
+	return std::find(_Observers.begin(), _Observers.end(), observer) != _Observers.end() ;
+}
+
+void		CParticleSystemInstanceUser::removePSObserver(IPSObserver *observer)
+{
+	nlassert(isPSObserver(observer)) ;
+	_Observers.erase(std::find(_Observers.begin(), _Observers.end(), observer)) ;
+}
+
+
+void		CParticleSystemInstanceUser::psDestroyed(CParticleSystemModel *psm)
+{
+	// the instance pointer is invalid now
+	_Transform = NULL ;
+	std::vector<IPSObserver *> obserCopy(_Observers.begin(), _Observers.end()) ;
+	for (std::vector<IPSObserver *>::iterator it = _Observers.begin(); it != _Observers.end() ; ++it)
+	{
+		(*it)->systemDestroyed(this) ;
+	}
+}
+
+
+void				CParticleSystemInstanceUser::getShapeAABBox(NLMISC::CAABBox &bbox) const
+{
+	bbox.setCenter(CVector::Null) ;
+	bbox.setHalfSize(CVector::Null) ;
+}
+
+uint				CParticleSystemInstanceUser::getNumMaterials() const
+{
+	return 0 ;
+}
+
+UInstanceMaterial	&CParticleSystemInstanceUser::getMaterial(uint materialId)
+{
+	nlassert(0) ; // no material for a particle system
+
+	// return dummy object
+	return *(UInstanceMaterial *) NULL ;
+}
+
+
+
+
+
+} // NL3D

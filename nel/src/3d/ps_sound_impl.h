1c24ed65443cc715a40fe8a9c3a50cd42bed69e2
Nicolas Vizerie <vizerie@nevrax.com>
2001-08-16 19:13:23 +0200

    #ADDED: implementation that make used of NEL SOUND

--- nel/src/3d/ps_sound_impl.h
+++ nel/src/3d/ps_sound_impl.h
@@ -1,7 +1,7 @@
 /** \file ps_sound_impl.h
  * <File description>
  *
- * $Id: ps_sound_impl.h,v 1.2 2001/08/09 08:03:19 vizerie Exp $
+ * $Id: ps_sound_impl.h,v 1.3 2001/08/16 17:13:23 vizerie Exp $
  */
 
 /* Copyright, 2000, 2001 Nevrax Ltd.
@@ -26,11 +26,71 @@
 #ifndef NL_PS_SOUND_IMPL_H
 #define NL_PS_SOUND_IMPL_H
 
+#include <string>
 #include "nel/sound/u_audio_mixer.h"
 #include "nel/misc/types_nl.h"
+#include "3d/ps_sound_interface.h"
 
 
-namespace NL3D {
+namespace NL3D 
+{
+
+
+/// This class implement a sound instance (a sound source)
+class CPSSoundInstanceImpl : public IPSSoundInstance
+{
+public:
+	/// construct this object from a nel sound source
+	/** The system will call this method to set the parameters of the sound	  
+	  */
+	CPSSoundInstanceImpl(NLSOUND::USource *source) : _Source(source)
+	{
+		
+	}
+
+	/// change this sound source paramerters
+	virtual void setSoundParams(float gain
+						   , const NLMISC::CVector &pos
+						   , const NLMISC::CVector &velocity
+						   , float pitch
+						  )
+	{
+		nlassert(_Source);
+		_Source->setPos(pos);
+		_Source->setVelocity(velocity);
+		_Source->setGain(gain);
+		_Source->setPitch(pitch);
+	}
+
+	/// start to play the sound
+	virtual void play(void)
+	{
+		nlassert(_Source);
+		_Source->play();
+	}
+
+	/// stop the sound
+	virtual void stop(void)
+	{
+		nlassert(_Source);
+		_Source->stop();
+	}
+
+	/// release the sound source
+	virtual void release(void)
+	{
+		delete _Source;
+		delete this;
+	}
+
+protected:
+	NLSOUND::USource *_Source;
+};
+
+
+
+
+
 
 
 /**
@@ -42,13 +102,49 @@ namespace NL3D {
 class CPSSoundServImpl : public IPSSoundServer
 {
 public:	
-	IPSSoundInstance *createSound(const std::string &soundName)
+	/// construct this sound server; You must init it then
+	CPSSoundServImpl() : _AudioMixer(NULL)
 	{
-		// for now, we just load a .wav
-		USoucre *source = createSource(
 	}
 
+	
+
+	/// init this particle system sound server, using the given audio mixer
+	void init(NLSOUND::UAudioMixer *audioMixer)
+	{
+		nlassert(audioMixer);
+		_AudioMixer = audioMixer;
+	}
+
+
+	/// get the audio mixer associated with that server
+	NLSOUND::UAudioMixer *getAudioMixer(void) { return _AudioMixer;}
+	const NLSOUND::UAudioMixer *getAudioMixer(void) const { return _AudioMixer;}
+
+
+	/// inherited from IPSSoundServer
+	IPSSoundInstance *createSound(const std::string &soundName)
+	{		
+		nlassert(_AudioMixer);
+		NLSOUND::USource *source = _AudioMixer->createSource(soundName.c_str());
+		if (source)
+		{			
+			return new CPSSoundInstanceImpl(source);
+		}
+		else
+		{
+			return NULL;
+		}
+	}
+
+protected:
+
+	NLSOUND::UAudioMixer  *_AudioMixer;
+
 };
+
+
+
 
 
 } // NL3D

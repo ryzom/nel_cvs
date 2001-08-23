2d87a9e150eb3fa0b345e84c9939c7417355dbc6
Nicolas Vizerie <vizerie@nevrax.com>
2001-08-23 16:18:51 +0200

    #ADDED: sound system

---
+++ nel/tools/3d/object_viewer/sound_system.cpp
@@ -0,0 +1,84 @@
+/** \file sound_system.cpp
+ * This initilize the sound system
+ *
+ * $Id: sound_system.cpp,v 1.1 2001/08/23 14:18:51 vizerie Exp $
+ */
+
+/* Copyright, 2001 Nevrax Ltd.
+ *
+ * This file is part of NEVRAX NEL.
+ * NEVRAX NEL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+
+ * NEVRAX NEL is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with NEVRAX NEL; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA.
+ */
+
+#include "std_afx.h"
+#include "resource.h"
+#include "sound_system.h"
+#include "nel/sound/u_audio_mixer.h"
+#include "nel/sound/u_listener.h"
+#include "nel/misc/path.h"
+#include "edit_ps_sound.h"
+
+NLSOUND::UAudioMixer *CSoundSystem::_AudioMixer = NULL;
+std::string			 CSoundSystem::_SoundBankFileName;
+
+
+
+void CSoundSystem::setListenerMatrix(const NLMISC::CMatrix &m)
+{
+	nlassert(_AudioMixer);
+	NLSOUND::UListener *l = _AudioMixer->getListener();
+	l->setPos(m.getPos());	
+	NLMISC::CVector j = m.getJ(), k = m.getK();
+	l->setOrientation(NLMISC::CVector(j.x, -j.z, j.y), NLMISC::CVector(k.x, -k.z, k.y));
+}
+
+
+void CSoundSystem::initSoundSystem(void)
+{		
+	_AudioMixer = NLSOUND::UAudioMixer::createAudioMixer();
+	_AudioMixer->init();
+	initPSSoundSystem(_AudioMixer);
+
+	if (_SoundBankFileName.size())
+	{
+		try
+		{
+			_AudioMixer->loadSoundBuffers(_SoundBankFileName.c_str());
+		}
+		catch (NLMISC::Exception &e)
+		{
+			std::string mess = "Unable to load sound file :" + _SoundBankFileName
+								+ "\n" + e.what();
+			::MessageBox(NULL, mess.c_str(), "stream error", MB_OK);
+		}
+	}
+}
+
+
+void CSoundSystem::poll()
+{
+	nlassert(_AudioMixer);
+	_AudioMixer->update();
+}
+
+
+
+void CSoundSystem::releaseSoundSystem(void)
+{
+	//	delete _AudioMixer;
+	_AudioMixer = NULL;
+}
+

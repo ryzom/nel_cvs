d9e3bb4c2f1a0a63fc25b2add3195edb86a0f809
Pierre Portier <portier@nevrax.com>
2001-10-30 16:57:29 +0100

    no message

--- nel/include/nel/ai/static_init.h
+++ nel/include/nel/ai/static_init.h
@@ -1,24 +1,24 @@
 /** \file static_init.h
  * <File description>
  *
- * $Id: static_init.h,v 1.3 2001/04/05 15:29:15 chafik Exp $
+ * $Id: static_init.h,v 1.4 2001/10/30 15:57:29 portier Exp $
  */
 
 /* Copyright, 2001 Nevrax Ltd.
  *
- * This file is part of NEVRAX NEL.
- * NEVRAX NEL is free software; you can redistribute it and/or modify
+ * This file is part of NEVRAX D.T.C. SYSTEM.
+ * NEVRAX D.T.C. SYSTEM is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2, or (at your option)
  * any later version.
 
- * NEVRAX NEL is distributed in the hope that it will be useful, but
+ * NEVRAX D.T.C. SYSTEM is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
 
  * You should have received a copy of the GNU General Public License
- * along with NEVRAX NEL; see the file COPYING. If not, write to the
+ * along with NEVRAX D.T.C. SYSTEM; see the file COPYING. If not, write to the
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
  * MA 02111-1307, USA.
  */

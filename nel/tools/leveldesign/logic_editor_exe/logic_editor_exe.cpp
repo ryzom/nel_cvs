b3b0a8ac67fce898a4800cb28931f69008176b41
Stéphane Coutelas <coutelas@nevrax.com>
2001-11-29 14:07:15 +0100

    Exe to launch the logic editor in stand alone mode

---
+++ nel/tools/leveldesign/logic_editor_exe/logic_editor_exe.cpp
@@ -0,0 +1,35 @@
+// logic_editor_exe.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "../logic_editor/logic_editor_interface.h"
+
+using namespace std;
+
+
+int APIENTRY WinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPSTR     lpCmdLine,
+                     int       nCmdShow)
+{
+ 	// Create a logic editor
+	ILogicEditor * logicEditor = ILogicEditor::getInterface();
+
+	if( logicEditor )
+	{
+		// Init ui
+		logicEditor->initUI();
+		//logicEditor->initUILight(0,0,100,100);
+
+		// Go
+		logicEditor->go();
+
+		// Release ui
+		logicEditor->releaseUI ();
+
+		// Delete the pointer
+		ILogicEditor::releaseInterface( logicEditor );
+	}
+
+	return 0;
+}

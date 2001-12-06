f6ced7f9bee7a5f272df97b9077a6d2bdb3f1d02
Matthieu Besson <besson@nevrax.com>
2001-12-06 12:25:03 +0100

    BUGFIXED: Long names copy

--- nel/tools/leveldesign/ld_3_make_distrib.bat
+++ nel/tools/leveldesign/ld_3_make_distrib.bat
@@ -6,30 +6,29 @@ REM --- Directories
 
 mkdir \\server\code\tools
 mkdir \\server\code\tools\leveldesign
-
-
+del \\server\code\tools\leveldesign\*.* /Q
 
 REM --- Upload Binaries
 
 REM - WorldEditor
 
-xcopy r:\code\nel\tools\3d\ligo\WorldEditor_Exe\releasedebug\WorldEditor.exe "\\server\code\tools\leveldesign" /D /Y
-xcopy r:\code\nel\lib\WorldEditor_release_debug.dll "\\server\code\tools\leveldesign" /D /Y
-xcopy r:\code\nel\lib\nel_drv_opengl_win_rd.dll "\\server\code\tools\leveldesign" /D /Y
+xcopy "r:\code\nel\tools\3d\ligo\WorldEditor_Exe\releasedebug\WorldEditor_exe.exe" "\\server\code\tools\leveldesign" /D /Y
+xcopy "r:\code\nel\lib\WorldEditor_release_debug.dll" "\\server\code\tools\leveldesign" /D /Y
+xcopy "r:\code\nel\lib\nel_drv_opengl_win_rd.dll" "\\server\code\tools\leveldesign" /D /Y
 
 REM - Georges
 
-xcopy r:\code\nel\tools\leveldesign\georges_exe\georges_release_debug.dll "\\server\code\tools\leveldesign" /D /Y
-xcopy r:\code\nel\tools\leveldesign\georges_exe\releasedebug\georges_exe.exe "\\server\code\tools\leveldesign" /D /Y
+xcopy "r:\code\nel\tools\leveldesign\georges_exe\georges_release_debug.dll" "\\server\code\tools\leveldesign" /D /Y
+xcopy "r:\code\nel\tools\leveldesign\georges_exe\releasedebug\georges_exe.exe" "\\server\code\tools\leveldesign" /D /Y
 
 REM - LogicEditor
 
-xcopy r:\code\nel\tools\leveldesign\logic_editor_exe\logic_editor_rd.dll "\\server\code\tools\leveldesign" /D /Y
-xcopy r:\code\nel\tools\leveldesign\logic_editor_exe\releasedebug\logic_editor_exe.exe "\\server\code\tools\leveldesign" /D /Y
+xcopy "r:\code\nel\tools\leveldesign\logic_editor_exe\logic_editor_rd.dll" "\\server\code\tools\leveldesign" /D /Y
+xcopy "r:\code\nel\tools\leveldesign\logic_editor_exe\releasedebug\logic_editor_exe.exe" "\\server\code\tools\leveldesign" /D /Y
 
 REM - Master
 
-xcopy r:\code\nel\tools\leveldesign\master\releasedebug\master.exe "\\server\code\tools\leveldesign" /D /Y
+xcopy "r:\code\nel\tools\leveldesign\master\releasedebug\master.exe" "\\server\code\tools\leveldesign" /D /Y
 
 
 

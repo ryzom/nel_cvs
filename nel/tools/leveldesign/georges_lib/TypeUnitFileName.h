b15de552bbcf3f74de09b8661eb81e65f172879a
urro <urro@nevrax.com>
2001-12-13 17:59:48 +0100

    aaa

---
+++ nel/tools/leveldesign/georges_lib/TypeUnitFileName.h
@@ -0,0 +1,27 @@
+// TypeUnitString.h: interface for the CTypeUnitString class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_TYPEUNITFILENAME_H__C32CF14C_50DE_4747_BC9E_4C1D088308FC__INCLUDED_)
+#define AFX_TYPEUNITFILENAME_H__C32CF14C_50DE_4747_BC9E_4C1D088308FC__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include "TypeUnit.h"
+
+class CTypeUnitFileName : public CTypeUnit  
+{
+protected:
+	unsigned short int	usihighlimit;
+
+public:
+	CTypeUnitFileName( const CStringEx _sxll, const CStringEx _sxhl, const CStringEx _sxdv, const CStringEx _sxf );
+	virtual ~CTypeUnitFileName();
+
+	virtual	CStringEx Format( const CStringEx _sxvalue ) const;									
+	virtual	CStringEx CalculateResult( const CStringEx _sxbasevalue, const CStringEx _sxvalue ) const;	
+};
+
+#endif // !defined(AFX_TYPEUNITSTRING_H__C32CF14C_50DE_4747_BC9E_4C1D088308FC__INCLUDED_)

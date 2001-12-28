e2fc6c628b6f3a8a9621e0d7c0965067191110ab
Matthieu Besson <besson@nevrax.com>
2001-12-28 15:48:55 +0100

    ADDED: Get the libs from server

--- nel/tools/leveldesign/ld_1_get.bat
+++ nel/tools/leveldesign/ld_1_get.bat
@@ -6,5 +6,7 @@ cvs -d :pserver:besson@server:/home/cvsroot update
 cd \code\ryzom\src
 cvs -d :pserver:besson@server:/home/cvsroot update
 
-xcopy "\\stephanec\gamedev\lib\*.*" "R:\distrib\gamedev\lib" /Y
+xcopy "\\server\code\distrib\gamedev\lib\logic_rd.lib" "R:\distrib\gamedev\lib" /Y
+xcopy "\\server\code\distrib\nel\lib\nlnet_rd.lib" "R:\distrib\nel\lib" /Y
+
 cd \code\nel\tools\leveldesign

a208c2b2a9e6d2142f7ada5b2b65a1db8519cc94
Vianney Lecroart <lecroart@nevrax.com>
2001-12-31 16:58:31 +0100

    ADDED: precomp header

---
+++ nel/src/3d/driver/opengl/stdopengl.h
@@ -0,0 +1,27 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+
+#include <string>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <sstream>
+#include <exception>
+#include <utility>
+#include <deque>
+
+#include "nel/misc/types_nl.h"
+#include "nel/misc/common.h"
+#include "nel/misc/debug.h"
+
+#include "nel/misc/stream.h"
+#include "nel/misc/mem_stream.h"
+#include "nel/misc/time_nl.h"
+#include "nel/misc/command.h"
+
+#include "driver_opengl.h"

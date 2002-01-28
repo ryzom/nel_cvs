b94cf30ce319a987536e257893fa8ff2a8ac2666
Nicolas Vizerie <vizerie@nevrax.com>
2002-01-28 15:23:53 +0100

    #ADDED: a manager that can force animation of systems that are not parsed during the clip traversal, and that allow to release resource of systems that are too far and not parsed

---
+++ nel/src/3d/particle_system_manager.cpp
@@ -0,0 +1,180 @@
+/** \file particle_system_manager.cpp
+ * <File description>
+ *
+ * $Id: particle_system_manager.cpp,v 1.1 2002/01/28 14:23:53 vizerie Exp $
+ */
+
+/* Copyright, 2000 - 2002 Nevrax Ltd.
+ *
+ * This file is part of NEVRAX NEL.
+ * NEVRAX NEL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+
+ * NEVRAX NEL is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with NEVRAX NEL; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA.
+ */
+
+#include "3d/particle_system_manager.h"
+#include "3d/particle_system_model.h"
+#include "3d/scene.h"
+
+namespace NL3D {
+
+///=========================================================	
+CParticleSystemManager::CParticleSystemManager() : _NumModels(0)
+{
+	_CurrListIterator = _ModelList.end();
+}
+
+///=========================================================	
+void	CParticleSystemManager::refreshModels(const std::vector<NLMISC::CPlane>	&worldFrustumPyramid,  const NLMISC::CVector &viewerPos)
+{	
+	#ifdef NL_DEBUG
+		nlassert(_NumModels == _ModelList.size());
+	#endif
+
+	if (_NumModels == 0) return;
+	const uint toProcess = std::min(_NumModels, (uint) NumProcessToRefresh);	
+
+	TModelList::iterator endIt = _ModelList.end();
+	for (uint k = 0; k < toProcess; ++k)
+	{
+		if (_CurrListIterator == _ModelList.end())
+		{
+			_CurrListIterator = _ModelList.begin();
+		}
+		
+		if ((*_CurrListIterator)->refreshRscDeletion(worldFrustumPyramid, viewerPos)) // check if the resources of the model must be released (which also release them)
+		{						
+			_CurrListIterator = _ModelList.erase(_CurrListIterator);			
+			-- _NumModels;
+		}
+		else
+		{
+			++ _CurrListIterator;
+		}		
+	}
+
+	#ifdef NL_DEBUG
+		nlassert(_NumModels == _ModelList.size());
+	#endif
+}
+
+///=========================================================	
+CParticleSystemManager::TModelHandle CParticleSystemManager::addSystemModel(CParticleSystemModel *model)
+{
+	#ifdef NL_DEBUG
+		nlassert(std::find(_ModelList.begin(), _ModelList.end(), model) == _ModelList.end()); 
+	#endif
+	_ModelList.push_front(model);
+	++_NumModels;
+
+	#ifdef NL_DEBUG
+		nlassert(_NumModels == _ModelList.size());
+	#endif
+
+	TModelHandle handle;
+	handle.Valid = true;
+	handle.Iter = _ModelList.begin();
+	return handle;
+}
+
+
+///=========================================================	
+void		CParticleSystemManager::removeSystemModel(TModelHandle &handle)
+{	
+	nlassert(handle.Valid);
+	#ifdef NL_DEBUG
+		nlassert(_NumModels == _ModelList.size());
+	#endif
+	nlassert(_NumModels != 0);	
+	if (handle.Iter == _CurrListIterator)
+	{
+		++_CurrListIterator;		
+	}
+
+	_ModelList.erase(handle.Iter);
+	--_NumModels;
+	#ifdef NL_DEBUG
+		nlassert(_NumModels == _ModelList.size());
+	#endif
+}
+
+
+///=========================================================	
+CParticleSystemManager::TModelHandle	CParticleSystemManager::addPermanentlyAnimatedSystem(CParticleSystemModel *ps)
+{
+	#ifdef NL_DEBUG
+		nlassert(std::find(_PermanentlyAnimatedModelList.begin(), _PermanentlyAnimatedModelList.end(), ps) == _PermanentlyAnimatedModelList.end());
+	#endif
+	_PermanentlyAnimatedModelList.push_front(ps);
+
+
+	TModelHandle handle;
+	handle.Valid = true;
+	handle.Iter = _PermanentlyAnimatedModelList.begin();
+	return handle;	
+}
+
+///=========================================================	
+void			CParticleSystemManager::removePermanentlyAnimatedSystem(CParticleSystemManager::TModelHandle &handle)
+{
+	nlassert(handle.Valid);
+	_PermanentlyAnimatedModelList.erase(handle.Iter);	
+}
+
+///=========================================================	
+void	CParticleSystemManager::processAnimate(TAnimationTime deltaT)
+{
+	for (TModelList::iterator it = _PermanentlyAnimatedModelList.begin(); it != _PermanentlyAnimatedModelList.end(); ++it)
+	{
+		CParticleSystemModel &psm = *(*it);
+		CParticleSystem		 *ps  = psm.getPS();
+		if (ps)
+		{
+			if (psm.isAutoGetEllapsedTimeEnabled())
+			{
+				psm.setEllapsedTime(ps->getScene()->getEllapsedTime());
+			}
+			TAnimationTime delay = psm.getEllapsedTime();
+			// animate particles
+			ps->step(CParticleSystem::Anim, delay);	
+
+			// test deletion condition (no more particle, no more particle and emitters)
+			if (ps->getDestroyCondition() != CParticleSystem::none)
+			{
+				if (ps->getSystemDate() > ps->getDelayBeforeDeathConditionTest())
+				{
+					switch (ps->getDestroyCondition())
+					{
+						case CParticleSystem::noMoreParticles:
+							if (!ps->hasParticles())
+							{							
+								psm.releaseRscAndInvalidate();
+								return;
+							}
+						break;
+						case CParticleSystem::noMoreParticlesAndEmitters:
+							if (!ps->hasParticles() && !ps->hasEmitters())
+							{
+								psm.releaseRscAndInvalidate();
+								return;
+							}
+						break;
+					}
+				}
+			}
+		}
+	}
+}
+
+} // NL3D
---
+++ nel/src/3d/particle_system_manager.h
@@ -0,0 +1,112 @@
+/** \file particle_system_manager.h
+ * <File description>
+ *
+ * $Id: particle_system_manager.h,v 1.1 2002/01/28 14:23:53 vizerie Exp $
+ */
+
+/* Copyright, 2000 - 2002 Nevrax Ltd.
+ *
+ * This file is part of NEVRAX NEL.
+ * NEVRAX NEL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+
+ * NEVRAX NEL is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with NEVRAX NEL; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA.
+ */
+
+#ifndef NL_PARTICLE_SYSTEM_MANAGER_H
+#define NL_PARTICLE_SYSTEM_MANAGER_H
+
+#include "nel/misc/types_nl.h"
+#include "nel/misc/plane.h"
+#include "nel/3d/animation_time.h"
+
+
+#include <list>
+#include <vector>
+
+namespace NLMISC
+{
+	class CVector;
+}
+
+namespace NL3D {
+
+
+class CParticleSystemModel;
+
+/**
+ * This class list all the particle systems that have resources allocated at a given time, so that 
+ * we can remove the resource of those who are too far from the viewer.
+ * This has been added because objects that are in a cluster that is not 
+ * visible are not parsed during traversals, so their resources couldn't just be released
+ * Only a few systems are parsed each time (because they can be numerous).
+ * Call to that class should be made by CScene, so you don't need to use it
+ * \author Nicolas Vizerie
+ * \author Nevrax France
+ * \date 2002
+ */
+class CParticleSystemManager
+{
+public:
+	enum { NumProcessToRefresh  = 3 }; // the number of systems that are refreshed at each call
+
+	/// default ctor
+	CParticleSystemManager();	
+
+	/// call this to refresh systems. (check those whose data should be released)
+	void	refreshModels(const std::vector<NLMISC::CPlane>	&worldFrustumPyramid,  const NLMISC::CVector &viewerPos);
+
+	/// perform animation on systems that should be animated even if not parsed (temporary spells for example)
+	void	processAnimate(TAnimationTime deltaT);
+
+private:
+	friend class CParticleSystemModel;
+	typedef std::list<CParticleSystemModel *> TModelList;
+
+	struct TModelHandle
+	{
+		TModelList::iterator Iter;
+		bool				 Valid;
+		TModelHandle() : Valid(false) {}
+	};
+
+	
+	/** Should be called when a new p.s. model has resources attached to it.
+	  * \see removeSystemModel
+	  */
+	TModelHandle addSystemModel(CParticleSystemModel *);
+
+	/// Should called when a p.s model has resources detached from it.
+	void			removeSystemModel(TModelHandle &handle);
+
+	/// Should be called to attach a system that must always be animated
+	TModelHandle addPermanentlyAnimatedSystem(CParticleSystemModel *);
+
+	/// Remove a permanenlty animated system
+	void			removePermanentlyAnimatedSystem(TModelHandle &handle);
+	
+private:
+
+	TModelList::iterator _CurrListIterator; /// the current element being processed
+	TModelList			 _ModelList;
+	TModelList			 _PermanentlyAnimatedModelList;
+	uint			     _NumModels;
+};
+
+
+} // NL3D
+
+
+#endif // NL_PARTICLE_SYSTEM_MANAGER_H
+
+/* End of particle_system_manager.h */

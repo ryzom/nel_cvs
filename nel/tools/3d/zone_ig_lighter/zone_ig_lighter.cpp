9cc22087d84215ad574e8f5847011820ddea8f53
Lionel Bérenguier <berenguier@nevrax.com>
2002-02-07 09:59:09 +0100

    ADDED: light ig of zones

---
+++ nel/tools/3d/zone_ig_lighter/zone_ig_lighter.cpp
@@ -0,0 +1,546 @@
+/** \file zone_ig_lighter.cpp
+ * zone_ig_lighter.cpp : instance lighter for ig in landscape zones
+ * greatly copied from ../zone_lighter/zone_lighter.cpp
+ *
+ * $Id: zone_ig_lighter.cpp,v 1.1 2002/02/07 08:59:09 berenguier Exp $
+ */
+
+/* Copyright, 2000 Nevrax Ltd.
+ *
+ * This file is part of NEVRAX NEL.
+ * NEVRAX NEL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+
+ * NEVRAX NEL is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with NEVRAX NEL; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA.
+ */
+
+#include "nel/misc/stream.h"
+#include "nel/misc/file.h"
+#include "nel/misc/vector.h"
+#include "nel/misc/time_nl.h"
+#include "nel/misc/config_file.h"
+#include "nel/misc/path.h"
+
+#include "3d/zone.h"
+#include "3d/instance_lighter.h"
+#include "3d/quad_grid.h"
+#include "3d/landscape.h"
+#include "3d/scene_group.h"
+#include "3d/shape.h"
+#include "3d/transform_shape.h"
+#include "3d/register_3d.h"
+
+#include "../zone_welder/zone_utility.h"	// load a header file from zone_welder project
+
+using namespace std;
+using namespace NLMISC;
+using namespace NL3D;
+
+#define BAR_LENGTH 21
+
+char *progressbar[BAR_LENGTH]=
+{
+	"[                    ]",
+	"[.                   ]",
+	"[..                  ]",
+	"[...                 ]",
+	"[....                ]",
+	"[.....               ]",
+	"[......              ]",
+	"[.......             ]",
+	"[........            ]",
+	"[.........           ]",
+	"[..........          ]",
+	"[...........         ]",
+	"[............        ]",
+	"[.............       ]",
+	"[..............      ]",
+	"[...............     ]",
+	"[................    ]",
+	"[.................   ]",
+	"[..................  ]",
+	"[................... ]",
+	"[....................]"
+};
+
+// My zone lighter
+class CMyIgZoneLighter : public CInstanceLighter
+{
+	// Progress bar
+	virtual void progress (const char *message, float progress)
+	{
+		// Progress bar
+		char msg[512];
+		uint	pgId= (uint)(progress*(float)BAR_LENGTH);
+		pgId= min(pgId, (uint)(BAR_LENGTH-1));
+		sprintf (msg, "\r%s: %s", message, progressbar[pgId]);
+		for (uint i=strlen(msg); i<79; i++)
+			msg[i]=' ';
+		msg[i]=0;
+		printf (msg);
+		printf ("\r");
+	}
+};
+
+
+
+
+int main(int argc, char* argv[])
+{
+	// Register 3d
+	registerSerial3d ();
+
+	// Good number of args ?
+	if (argc<5)
+	{
+		// Help message
+		printf ("%s [zonein.zonel] [igout.ig] [parameter_file] [dependancy_file]\n", argv[0]);
+	}
+	else
+	{
+		// Ok, read the zone
+		CIFile inputFile;
+
+		// Get extension
+		string ext=getExt (argv[1]);
+		string dir=getDir (argv[1]);
+
+		// Open it for reading
+		if (inputFile.open (argv[1]))
+		{
+			// Zone name
+			string zoneName=strlwr (string ("zone_"+getName (argv[1])));
+
+			// Load the zone
+			try
+			{
+				// Read the config file
+				CConfigFile parameter;
+				CConfigFile dependency;
+
+				// Load and parse the parameter file
+				parameter.load (argv[3]);
+
+				// Load and parse the dependency file
+				dependency.load (argv[4]);
+
+				// Fill the pathes
+				string ig_path = parameter.getVar ("ig_path").asString();
+				if ((ig_path[ig_path.length()-1]!='\\')&&(ig_path[ig_path.length()-1]!='/'))
+					ig_path+="/";
+				string shapes_path = parameter.getVar ("shapes_path").asString();
+				if ((shapes_path[shapes_path.length()-1]!='\\')&&(shapes_path[shapes_path.length()-1]!='/'))
+					shapes_path+="/";
+
+				// Get the maps path
+				string maps_path = parameter.getVar ("maps_path").asString();
+
+				// Add it to the current path
+				CPath::addSearchPath (maps_path);
+
+
+				// A landscape allocated with new: it is not delete because destruction take 3 secondes more!
+				CLandscape *landscape=new CLandscape;
+				landscape->init();
+
+				// A zone lighter
+				CMyIgZoneLighter lighter;
+				lighter.init ();
+
+				// A vector of zone id
+				vector<uint> listZoneId;
+
+				// The zone
+				CZone zone;
+
+				// List of ig
+				std::list<CInstanceGroup*> instanceGroup;
+
+				// Load
+				zone.serial (inputFile);
+				inputFile.close();
+
+				// Load ig of the zone
+				string igName=ig_path+getName (argv[1])+".ig";
+
+				bool zoneIgLoaded;
+
+				// Try to open the file
+				CInstanceGroup *centerInstanceGroup= NULL;
+				if (inputFile.open (igName))
+				{
+					// load the center ig
+					centerInstanceGroup=new CInstanceGroup;
+
+					// Serial it
+					centerInstanceGroup->serial (inputFile);
+					inputFile.close();
+
+					// Add to the list
+					instanceGroup.push_back (centerInstanceGroup);
+					zoneIgLoaded = true;
+				}
+				else
+				{
+					// Warning
+					fprintf (stderr, "Warning: can't load instance group %s\n", igName.c_str());
+					zoneIgLoaded = false;
+				}
+
+				// If can't load the center instanceGroup, skip it.
+				if(!zoneIgLoaded)
+					return 0;
+
+				// Get bank path
+				CConfigFile::CVar &bank_path = parameter.getVar ("bank_path");
+
+				// Load the bank
+				if (inputFile.open (bank_path.asString()))
+				{
+					try
+					{
+						// Load
+						landscape->TileBank.serial (inputFile);
+						landscape->initTileBanks();
+					}
+					catch (Exception &e)
+					{
+						// Error
+						nlwarning ("ERROR error loading tile bank %s\n%s\n", bank_path.asString().c_str(), e.what());
+					}
+				}
+				else
+				{
+					// Error
+					nlwarning ("ERROR can't load tile bank %s\n", bank_path.asString().c_str());
+				}
+
+				// Add the zone
+				landscape->addZone (zone);
+				listZoneId.push_back (zone.getZoneId());
+
+				// Load instance group ?
+				CConfigFile::CVar &load_ig= parameter.getVar ("load_ig");
+				bool loadInstanceGroup = load_ig.asInt ()!=0;
+
+				// Continue to build ?
+				bool continu=true;
+
+				// Try to load additionnal instance group.
+				if (loadInstanceGroup)
+				{
+					// Additionnal instance group
+					try
+					{
+						CConfigFile::CVar &additionnal_ig = parameter.getVar ("additionnal_ig");									
+						for (uint add=0; add<(uint)additionnal_ig.size(); add++)
+						{
+							// Input file
+							CIFile inputFile;
+
+							// Name of the instance group
+							string name=ig_path+additionnal_ig.asString(add);
+
+							// Try to open the file
+							if (inputFile.open (name))
+							{
+								// New ig
+								CInstanceGroup *group=new CInstanceGroup;
+
+								// Serial it
+								group->serial (inputFile);
+								inputFile.close();
+
+								// Add to the list
+								instanceGroup.push_back (group);
+							}
+							else
+							{
+								// Error
+								nlwarning ("ERROR can't load instance group %s\n", name.c_str());
+
+								// Stop before build
+								continu=false;
+							}
+						}
+					}
+					catch (NLMISC::EUnknownVar &)
+					{
+						nlinfo("No additionnal ig's to load");
+					}
+				}
+				
+				// *** Scan dependency file
+				CConfigFile::CVar &dependant_zones = dependency.getVar ("dependencies");
+				for (uint i=0; i<(uint)dependant_zones.size(); i++)
+				{
+					// Get zone name
+					string zoneName=dependant_zones.asString(i);
+
+					// Load the zone
+					CZone zoneBis;
+
+					// Open it for reading
+					if (inputFile.open (dir+zoneName+ext))
+					{
+						// Read it
+						zoneBis.serial (inputFile);
+						inputFile.close();
+
+						// Add the zone
+						landscape->addZone (zoneBis);
+						listZoneId.push_back (zoneBis.getZoneId());
+					}
+					else
+					{
+						// Error message and continue
+						nlwarning ("ERROR can't load zone %s\n", (dir+zoneName+ext).c_str());
+					}
+
+					// Try to load an instance group.
+					if (loadInstanceGroup)
+					{
+						string name=ig_path+zoneName+".ig";
+
+						// Name of the instance group
+						if (inputFile.open (name))
+						{
+							// New ig
+							CInstanceGroup *group=new CInstanceGroup;
+
+							// Serial it
+							group->serial (inputFile);
+							inputFile.close();
+
+							// Add to the list
+							instanceGroup.push_back (group);
+						}
+						else
+						{
+							// Error message and continue
+							nlwarning ("WARNING can't load instance group %s\n", name.c_str());
+						}
+					}
+				}
+
+				// **********
+				// *** Build the lighter descriptor
+				// **********
+
+				CInstanceLighter::CLightDesc lighterDesc;
+
+				// shapes_path
+				lighterDesc.ShapePath= shapes_path;
+
+				// Light direction
+				CConfigFile::CVar &sun_direction = parameter.getVar ("sun_direction");
+				lighterDesc.LightDirection.x=sun_direction.asFloat(0);
+				lighterDesc.LightDirection.y=sun_direction.asFloat(1);
+				lighterDesc.LightDirection.z=sun_direction.asFloat(2);
+				lighterDesc.LightDirection.normalize ();
+
+				// Grid size
+				CConfigFile::CVar &quad_grid_size = parameter.getVar ("quad_grid_size");
+				lighterDesc.GridSize=quad_grid_size.asInt();
+
+				// Grid size
+				CConfigFile::CVar &quad_grid_cell_size = parameter.getVar ("quad_grid_cell_size");
+				lighterDesc.GridCellSize=quad_grid_cell_size.asFloat();
+
+				// Shadows enabled ?
+				CConfigFile::CVar &shadow = parameter.getVar ("shadow");
+				lighterDesc.Shadow=shadow.asInt ()!=0;
+
+				// OverSampling
+				CConfigFile::CVar &ig_oversampling = parameter.getVar ("ig_oversampling");
+				lighterDesc.OverSampling= ig_oversampling.asInt ();
+				// validate value: 0, 2, 4, 8, 16
+				lighterDesc.OverSampling= raiseToNextPowerOf2(lighterDesc.OverSampling);
+				clamp(lighterDesc.OverSampling, 0U, 16U);
+				if(lighterDesc.OverSampling<2)
+					lighterDesc.OverSampling= 0;
+
+
+				// A vector of CInstanceLighter::CTriangle
+				vector<CInstanceLighter::CTriangle> vectorTriangle;
+
+				// **********
+				// *** Build triangle array
+				// **********
+
+				landscape->checkBinds ();
+
+				// Add triangles from landscape, for pointLight lighting.
+				landscape->enableAutomaticLighting (false);
+				lighter.addTriangles (*landscape, listZoneId, 0, vectorTriangle);
+
+				// Load and add shapes
+
+				// Map of shape
+				std::map<string, IShape*> shapeMap;
+
+				// For each instance group
+				std::list<CInstanceGroup*>::iterator ite=instanceGroup.begin();
+				while (ite!=instanceGroup.end())
+				{
+					// Instance group
+					CInstanceGroup *group=*ite;
+
+					// For each instance
+					for (uint instance=0; instance<group->getNumInstance(); instance++)
+					{
+						// Get the instance shape name
+						string name=shapes_path+group->getShapeName (instance);
+
+						// Skip it??
+						if(group->getInstance(instance).DontCastShadow)
+							continue;
+
+
+						// Add a .shape at the end ?
+						if (name.find('.') == std::string::npos)
+							name += ".shape";
+
+						// Find the shape in the bank
+						std::map<string, IShape*>::iterator iteMap=shapeMap.find (name);
+						if (iteMap==shapeMap.end())
+						{
+							// Input file
+							CIFile inputFile;
+
+							if (inputFile.open (name))
+							{
+								// Load it
+								CShapeStream stream;
+								stream.serial (inputFile);
+
+								// Get the pointer
+								iteMap=shapeMap.insert (std::map<string, IShape*>::value_type (name, stream.getShapePointer ())).first;
+							}
+							else
+							{
+								// Error
+								nlwarning ("WARNING can't load shape %s\n", name.c_str());
+							}
+						}
+						
+						// Loaded ?
+						if (iteMap!=shapeMap.end())
+						{
+							// Build the matrix
+							CMatrix scale;
+							scale.identity ();
+							scale.scale (group->getInstanceScale (instance));
+							CMatrix rot;
+							rot.identity ();
+							rot.setRot (group->getInstanceRot (instance));
+							CMatrix pos;
+							pos.identity ();
+							pos.setPos (group->getInstancePos (instance));
+							CMatrix mt=pos*rot*scale;
+
+							// If centerInstanceGroup, take good instanceId, to avoid selfShadowing
+							sint	instanceId;
+							if(group == centerInstanceGroup)
+								instanceId= instance;
+							else
+								instanceId= -1;
+
+							// Add triangles
+							lighter.addTriangles (*iteMap->second, mt, vectorTriangle, instanceId);
+						}
+					}
+
+					// For each point light of the ig
+					const std::vector<CPointLightNamed>	&pointLightList= group->getPointLightList();
+					for (uint plId=0; plId<pointLightList.size(); plId++)
+					{
+						// Add it to the Ig.
+						lighter.addStaticPointLight(pointLightList[plId]);
+					}
+
+					// Next instance group
+					ite++;
+				}
+
+				// Continue ?
+				if (continu)
+				{
+					// **********
+					// *** Light!
+					// **********
+
+					// Start time
+					TTime time=CTime::getLocalTime ();
+
+					// Output ig
+					CInstanceGroup	output;
+
+					// Light the zone
+					lighter.light (*centerInstanceGroup, output, lighterDesc, vectorTriangle, landscape);
+
+					// Compute time
+					printf ("\rCompute time: %d ms                                                      \r", 
+						(uint)(CTime::getLocalTime ()-time));
+
+					// Save the zone
+					COFile outputFile;
+
+					// Open it
+					if (outputFile.open (argv[2]))
+					{
+						try
+						{
+							// Save the new ig
+							outputFile.serial(output);
+						}
+						catch (Exception& except)
+						{
+							// Error message
+							nlwarning ("ERROR writing %s: %s\n", argv[2], except.what());
+						}
+					}
+					else
+					{
+						// Error can't open the file
+						nlwarning ("ERROR Can't open %s for writing\n", argv[2]);
+					}
+				}
+				else
+				{
+					// Error
+					nlwarning ("ERROR Abort: files are missing.\n");
+				}
+			}
+			catch (Exception& except)
+			{
+				// Error message
+				nlwarning ("ERROR %s\n", except.what());
+			}
+		}
+		else
+		{
+			// Error can't open the file
+			nlwarning ("ERROR Can't open %s for reading\n", argv[1]);
+		}
+
+	}
+	
+
+	// Landscape is not deleted, nor the instanceGroups, for faster quit.
+	// Must disalbe BlockMemory checks (for pointLights).
+	NL3D_BlockMemoryAssertOnPurge= false;
+
+	// exit.
+	return 0;
+}

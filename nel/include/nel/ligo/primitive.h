fc4509d44868d6094f520c0fda641d08fa09df69
Matthieu Besson <besson@nevrax.com>
2002-02-14 14:11:13 +0100

    ADDED: First version

---
+++ nel/include/nel/ligo/primitive.h
@@ -0,0 +1,103 @@
+/** \file primitive.h
+ * <File description>
+ *
+ * $Id: primitive.h,v 1.1 2002/02/14 13:11:13 besson Exp $
+ */
+
+/* Copyright, 2001 Nevrax Ltd.
+ *
+ * This file is part of NEVRAX NEL.
+ * NEVRAX NEL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+
+ * NEVRAX NEL is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with NEVRAX NEL; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA.
+ */
+
+#ifndef __PRIMITIVE_H__
+#define __PRIMITIVE_H__
+
+#include "nel/misc/vector.h"
+#include <vector>
+
+namespace NLLIGO
+{
+
+// There are only x and y used in the vector
+
+
+// ***************************************************************************
+class CPrimPoint
+{
+
+public:
+
+	NLMISC::CVector		Point;
+	std::string			Name;
+	std::string			LayerName;
+
+public:
+
+	void serial (NLMISC::IStream &f);
+};
+
+// ***************************************************************************
+class CPrimPath
+{
+
+public:
+
+	std::vector<NLMISC::CVector>	VPoints;
+	std::string						Name;
+	std::string						LayerName;
+
+public:
+
+	void serial (NLMISC::IStream &f);
+};
+
+
+// ***************************************************************************
+class CPrimZone
+{
+
+public:
+
+	std::vector<NLMISC::CVector>	VPoints;
+	std::string						Name;
+	std::string						LayerName;
+
+public:
+
+	bool contains (NLMISC::CVector &v);
+	void serial (NLMISC::IStream &f);
+};
+
+// ***************************************************************************
+class CPrimRegion
+{
+
+public:
+
+	std::string				Name;
+	std::vector<CPrimPoint> VPoints;
+	std::vector<CPrimZone>	VZones;
+	std::vector<CPrimPath>	VPaths;
+
+public:
+
+	void serial (NLMISC::IStream &f);
+};
+
+} // namespace NLLIGO
+
+#endif // __PRIMITIVE_H__

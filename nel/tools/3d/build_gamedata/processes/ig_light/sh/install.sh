687670632cd5808d7a10e689760e89bc4c514dea
Lionel Bérenguier <berenguier@nevrax.com>
2002-02-19 10:19:34 +0100

    ADDED: install

---
+++ nel/tools/3d/build_gamedata/processes/ig_light/sh/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Install ig in the client data
+
+# Get the ig install directory
+ig_install_directory=`cat ../../cfg/directories.cfg | grep "ig_install_directory" | sed -e 's/ig_install_directory//' | sed -e 's/ //g' | sed -e 's/=//g'`
+
+# Get the maps install directory
+map_install_directory=`cat ../../cfg/directories.cfg | grep "map_install_directory" | sed -e 's/map_install_directory//' | sed -e 's/ //g' | sed -e 's/=//g'`
+
+# Get the client directory
+client_directory=`cat ../../cfg/config.cfg | grep "client_directory" | sed -e 's/client_directory//' | sed -e 's/ //g' | sed -e 's/=//g'`
+
+# Log error
+echo ------- > log.log
+echo --- Install Lighted Ig >> log.log
+echo ------- >> log.log
+echo ------- 
+echo --- Install Lighted Ig 
+echo ------- 
+
+cp -u ig_other_lighted/*.ig $client_directory/$ig_install_directory  2>> log.log
+

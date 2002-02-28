13fde6dd73bbfac9576a99dbffd1820dbbdc788b
Nicolas Vizerie <vizerie@nevrax.com>
2002-02-28 10:55:20 +0100

    #ADDED: missing GPL header

--- nel/src/3d/water_height_map.h
+++ nel/src/3d/water_height_map.h
@@ -1,3 +1,29 @@
+/** \file water_height_map.h
+ * <File description>
+ *
+ * $Id: water_height_map.h,v 1.7 2002/02/28 09:55:20 vizerie Exp $
+ */
+
+/* Copyright, 2000, 2001 Nevrax Ltd.
+ *
+ * This file is part of NEVRAX NEL.
+ * NEVRAX NEL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+
+ * NEVRAX NEL is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with NEVRAX NEL; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA.
+ */
+
+
 #ifndef WATER_HEIGHT_MAP_H
 #define WATER_HEIGHT_MAP_H
 

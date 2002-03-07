fd155513db4a5d18d19ca0df2d93a1a59f3bfdac
Matthieu Besson <besson@nevrax.com>
2002-03-07 11:52:46 +0100

    ADDED: weld the zone

--- nel/tools/3d/build_gamedata/processes/ligo/sh/build.sh
+++ nel/tools/3d/build_gamedata/processes/ligo/sh/build.sh
@@ -67,28 +67,125 @@ dir_current=`pwd`
 land_name=`cat ../../cfg/config.cfg | grep "ligo_export_land" | sed -e 's/ligo_export_land//' | sed -e 's/ //g' | sed -e 's/=//g'`
 echo "ZoneRegionFile = \"$dir_database/$dir_ligosrc/$land_name\";" >> land_exporter.cfg
 
-# ok the CFG is generated
+# if there is no .land then do not generate all zones and zone welded
+
+if test -z "$land_name" ; then
+	echo No .land set -- Exiting --
+	exit;
+fi
 
 # *******************
 # launch the exporter
 # *******************
 
+echo Exporting
 ../../bin/land_export.exe land_exporter.cfg
 
 # rename *.zonel *.zone
+# script is just too slow to do renaming... And we can't call directly dos command
 
 cd output
 ../sh/renametozone.bat
 cd ..
 
-# script is just too slow to do renaming...
+# **************
+# Weld the zones
+# **************
 
-#dir_current=`pwd`
-#cd output
-#list_zonel=`ls -1 *.zonel`
-#for filename in $list_zonel ; do
-#	new_filename=`echo $filename|sed -e 's/zonel/zone/g'`
-#	mv $filename $new_filename
-#	echo "renaming $filename $new_filename"
-#done
-#cd $dir_current
+cd ../zone
+mkdir zone_exported
+mkdir zone_welded
+mkdir zone_depend
+
+# Try to copy ligo zone if any
+# ****************************
+
+ligo_flag=`cat ../../cfg/config.cfg | grep "process_to_complete" | grep "ligo"`
+
+dir_current=`pwd`
+cd ../ligo/output
+list_zone=`ls -1 *.zone`
+for filename in $list_zone ; do
+	echo "Checking $filename for update"
+	if test -e ../../zone/zone_exported/$filename ; then
+		must_update=`diff --binary -q $filename ../../zone/zone_exported/$filename` ;
+	else
+		must_update=YES ;
+	fi
+	
+	if test -n "$must_update" ; then
+		echo "   Updating"
+		cp -u $filename ../../zone/zone_exported/$filename ;
+	fi
+done
+cd $dir_current
+
+# delete files only present in the zone_exported directory
+
+if ( test "$ligo_flag" )
+then
+	cd ./zone_exported
+	list_zone=`ls -1 *.zone`
+	for filename in $list_zone ; do
+		if test -e ../../ligo/output/$filename ; then
+			must_update=NO ;
+		else
+			echo "Removing $filename"
+			rm $filename ;
+		fi
+	done
+	cd ..
+fi
+
+# ****************************
+
+# Build zone
+
+zone_dependencies='../../bin/zone_dependencies.exe'
+zone_welder='../../bin/zone_welder.exe'
+
+# **** Build dependencies
+
+# Log error
+echo ------- >> log.log
+echo --- Build zone : dependencies >> log.log
+echo ------- >> log.log
+echo ------- 
+echo --- Build zone : dependencies 
+echo ------- 
+
+# list all the dependencies regions
+zone_regions=`cat ../../cfg/config.cfg | grep "zone_region" | sed -e 's/zone_region//' | sed -e 's/ //g' | sed -e 's/=//g'`
+
+# For each dependencies region
+for i in $zone_regions ; do
+	# Extract the name
+	arg=`echo zone_exported/$zone_regions | sed -e 's&,&.zone zone_exported/&g'`
+
+	# Make the dependencies
+	$zone_dependencies ../../cfg/properties.cfg $arg.zone zone_depend/doomy.depend
+done
+
+# **** Weld
+
+# Log error
+echo ------- >> log.log
+echo --- Build zone : weld >> log.log
+echo ------- >> log.log
+echo ------- 
+echo --- Build zone : weld 
+echo ------- 
+
+# List the zones to weld
+list_zone=`ls -1 zone_exported/*.zone`
+
+# Weld zones
+for i in $list_zone ; do
+  dest=`echo $i | sed -e 's/zone_exported/zone_welded/g' | sed -e 's/.zone/.zonew/g'`
+  if ( ! test -e $dest ) || ( test $i -nt $dest )
+  then
+    echo -- Weld $i
+    $zone_welder $i $dest
+	echo 
+  fi
+done

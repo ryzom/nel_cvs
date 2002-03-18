23d912351d3f4beeed15031c8e05f1bc38845b24
Cyril Corvazier <corvazier@nevrax.com>
2002-03-18 18:28:31 +0100

    ADDED: font export

---
+++ nel/tools/3d/build_gamedata/processes/font/sh/export.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# *** Export fonts
+
+# Get the database directory
+database_directory=`cat ../../cfg/site.cfg | grep "database_directory" | sed -e 's/database_directory//g' | sed -e 's/ //g' | sed -e 's/=//g'`
+
+# Get the ps directories
+font_source_directories=`cat ../../cfg/directories.cfg | grep "font_source_directories" | sed -e 's/font_source_directories//' | sed -e 's/ //g' | sed -e 's/=//g'`
+
+# Log error
+echo ------- > log.log
+echo --- Export fonts >> log.log
+echo ------- >> log.log
+echo ------- 
+echo --- Export fonts
+echo ------- 
+date >> log.log
+date
+
+# For each font directory
+for i in $font_source_directories ; do
+	# Copy
+	cp -u $database_directory/$i/*.ttf fonts 2>> log.log
+	cp -u $database_directory/$i/*.afm fonts 2>> log.log
+	cp -u $database_directory/$i/*.pfb fonts 2>> log.log
+	cp -u $database_directory/$i/*.pfm fonts 2>> log.log
+done

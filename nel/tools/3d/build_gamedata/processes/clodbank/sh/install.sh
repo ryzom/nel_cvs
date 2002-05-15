5ffb5362c07c3e97eef20c98228fcd2c7cc3ab22
Lionel Bérenguier <berenguier@nevrax.com>
2002-05-15 14:02:53 +0200

    ADDED: install of clodbank

--- nel/tools/3d/build_gamedata/processes/clodbank/sh/install.sh
+++ nel/tools/3d/build_gamedata/processes/clodbank/sh/install.sh
@@ -1,30 +1,21 @@
 #!/bin/bash
 
-# Install shapes in the client data
+# Install clodbank in the client data, in the "shapes/" directory
 
 # Get the shape install directory
 shape_install_directory=`cat ../../cfg/directories.cfg | grep "shape_install_directory" | sed -e 's/shape_install_directory//' | sed -e 's/ //g' | sed -e 's/=//g'`
-
-# Get the maps install directory
-bitmap_install_directory=`cat ../../cfg/directories.cfg | grep "bitmap_install_directory" | sed -e 's/bitmap_install_directory//' | sed -e 's/ //g' | sed -e 's/=//g'`
-
-# Get the lightmaps install directory
-lightmap_install_directory=`cat ../../cfg/directories.cfg | grep "lightmap_install_directory" | sed -e 's/lightmap_install_directory//' | sed -e 's/ //g' | sed -e 's/=//g'`
 
 # Get the client directory
 client_directory=`cat ../../cfg/site.cfg | grep "client_directory" | sed -e 's/client_directory//' | sed -e 's/ //g' | sed -e 's/=//g'`
 
 # Log error
 echo ------- > log.log
-echo --- Install Shape >> log.log
+echo --- Install clodbank >> log.log
 echo ------- >> log.log
 echo ------- 
-echo --- Install Shape 
+echo --- Install clodbank 
 echo ------- 
 date >> log.log
 date
 
-cp -u -p shape/*.shape $client_directory/$shape_install_directory  2>> log.log
-cp -u -p shape_with_coarse_mesh_builded/*.shape $client_directory/$shape_install_directory  2>> log.log
-cp -u -p shape_with_coarse_mesh_builded/*.dds $client_directory/$bitmap_install_directory  2>> log.log
-cp -u -p lightmap_16_bits/*.tga $client_directory/$lightmap_install_directory  2>> log.log
+cp -u -p clodbank/*.clodbank $client_directory/$shape_install_directory  2>> log.log

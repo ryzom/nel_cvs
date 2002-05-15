116cbc7b0675341ebb031fea50710cfe8a459c23
Lionel Bérenguier <berenguier@nevrax.com>
2002-05-15 12:27:07 +0200

    FIXED: first workable version

--- nel/tools/3d/build_gamedata/processes/clodbank/sh/build.sh
+++ nel/tools/3d/build_gamedata/processes/clodbank/sh/build.sh
@@ -1,164 +1,25 @@
 #!/bin/bash
 
-# *** Build shape files (.shape)
-
-# Bin
-tga_2_dds='../../bin/tga2dds.exe'
-build_coarse_mesh='../../bin/build_coarse_mesh.exe'
-lightmap_optimizer='../../bin/lightmap_optimizer.exe'
-
-# Log error
-echo ------- > log.log
-echo --- Build shape : optimize lightmaps >> log.log
-echo ------- >> log.log
-echo ------- 
-echo --- Build shape : optimize lightmaps
-echo ------- 
-date >> log.log
-date
-
-# backup lightmap to lightmap_not_optimized and shape to shape_not_optimized
-# only if more recent
-
-for filename in lightmap/*.tga ; do
-	dest=`echo $filename | sed -e 's&lightmap/&lightmap_not_optimized/&g'`
-	if ( ! test -e $dest ) || ( test $filename -nt $dest )
-	then
-		what=`cp -v -u -p $filename $dest`
-		echo "$what"
-		echo "$what" >> log.log
-	fi
-done
-for filename in shape/*.shape ; do
-	dest=`echo $filename | sed -e 's&shape/&shape_not_optimized/&g'`
-	if ( ! test -e $dest ) || ( test $filename -nt $dest )
-	then
-		what=`cp -v -u -p $filename $dest`
-		echo "$what"
-		echo "$what" >> log.log
-	fi
-done
-
-$lightmap_optimizer ./lightmap ./shape
-
-for filename in lightmap_not_optimized/*.tga ; do
-	touch $filename
-done
-for filename in shape_not_optimized/*.shape ; do
-	touch $filename
-done
-
-
-echo ------- >> log.log
-echo --- Build shape : convert lightmaps in 16 bits >> log.log
-echo ------- >> log.log
-echo ------- 
-echo --- Build shape : convert lightmaps in 16 bits 
-echo ------- 
-date >> log.log
-date
-
-# For each directoy
-
-for i in lightmap/*.tga ; do
-
-	if ( test -f $i )
-	then
-		# Destination file
-		dest=`echo $i | sed -e 's/lightmap/lightmap_16_bits/g'`
-
-		# Convert the lightmap in 16 bits mode
-		if ( ! test -e $dest ) || ( test $i -nt $dest )
-		then
-			$tga_2_dds $i -o $dest -a tga16 2>> log.log
-		fi
-	fi
-done
-
 # Log error
 echo ------- >> log.log
-echo --- Build shape : build coarse meshes >> log.log
+echo --- Build clod : build .clodbank >> log.log
 echo ------- >> log.log
 echo ------- 
-echo --- Build shape : build coarse meshes 
+echo --- Build clod : build .clodbank
 echo ------- 
 date >> log.log
 date
-
-# Get the build gamedata directory
-build_gamedata_directory=`cat ../../cfg/site.cfg | grep "build_gamedata_directory" | sed -e 's/build_gamedata_directory//' | sed -e 's/ //g' | sed -e 's/=//g'`
 
 # Get the database directory
 database_directory=`cat ../../cfg/site.cfg | grep "database_directory" | sed -e 's/database_directory//g' | sed -e 's/ //g' | sed -e 's/=//g'`
 
-# Get texture pathes
-map_source_directories=`cat ../../cfg/directories.cfg | grep "map_source_directory" | sed -e 's/map_source_directory//' | sed -e 's/ //g' | sed -e 's/=//g'`
+# Get the lod config file in the database
+clod_config_file=`cat ../../cfg/config.cfg | grep "clod_config_file" | sed -e 's/clod_config_file//' | sed -e 's/ //g' | sed -e 's/=//g'`
 
-# Get the texture mul size
-texture_mul_size_value=`cat ../../cfg/config.cfg | grep "texture_mul_size_value" | sed -e 's/texture_mul_size_value//' | sed -e 's/ //g' | sed -e 's/=//g'`
+# Get the Lod character bank name
+clod_bank_file_name=`cat ../../cfg/config.cfg | grep "clod_bank_file_name" | sed -e 's/clod_bank_file_name//' | sed -e 's/ //g' | sed -e 's/=//g'`
 
-# Get the coarse mesh texture name
-coarse_mesh_texture_name=`cat ../../cfg/config.cfg | grep "coarse_mesh_texture_name" | sed -e 's/coarse_mesh_texture_name//' | sed -e 's/ //g' | sed -e 's/=//g'`
-
-# Copy the config file header
-cat cfg/config_header.cfg | sed -e "s/texture_mul_size_value/$texture_mul_size_value/g" > cfg/config_generated.cfg
-
-# Add the shape directory
-echo '	"'shape_with_coarse_mesh'"', >> cfg/config_generated.cfg
-
-# For each texture path
-for i in $map_source_directories ; do
-	
-	# Add the path
-	echo '	"'$database_directory/$i'"', >> cfg/config_generated.cfg
-
-done
-
-# Add the shape list header
-echo '};' >> cfg/config_generated.cfg
-echo ' ' >> cfg/config_generated.cfg
-echo 'list_mesh =' >> cfg/config_generated.cfg
-echo '{' >> cfg/config_generated.cfg
-
-# For each shape with coarse mesh
-for i in shape_with_coarse_mesh/*.shape; do
-	
-	if ( test -f $i )
-	then
-		# Destination file
-		src=`echo $i | sed -e 's&shape_with_coarse_mesh/&&g'`
-		dest=`echo $i | sed -e 's&shape_with_coarse_mesh&shape_with_coarse_mesh_builded&g'`
-
-		# Add the shape
-		echo '	"'$src'", "'$dest'",' >> cfg/config_generated.cfg
-
-		# Destination file
-		dest=`echo $i | sed -e 's/lightmap/lightmap_16_bits/g'`
-	fi
-
-done
-
-# Close the config file
-echo '};' >> cfg/config_generated.cfg
 
 # Execute the build
-$build_coarse_mesh cfg/config_generated.cfg 
+../../bin/build_clod_bank.exe  cfg/local_path.cfg  $database_directory/$clod_config_file  clodbank/$clod_bank_file_name
 
-# Log error
-echo ------- >> log.log
-echo --- Build shape : convert coarse texture to dds without mipmaps >> log.log
-echo ------- >> log.log
-echo ------- 
-echo --- Build shape : convert coarse texture to dds without mipmaps 
-echo ------- 
-date >> log.log
-date
-
-if ( test -f shape_with_coarse_mesh/nel_coarse_texture.tga )
-then
-	# Convert the coarse texture to dds
-	$tga_2_dds shape_with_coarse_mesh/nel_coarse_texture.tga -o shape_with_coarse_mesh_builded/nel_coarse_texture_builded.dds -a 5 2>> log.log
-
-	# Rename the coarse mesh texture
-	mv shape_with_coarse_mesh_builded/nel_coarse_texture_builded.dds "shape_with_coarse_mesh_builded/"$coarse_mesh_texture_name".dds"
-fi
--- nel/tools/3d/build_gamedata/processes/clodbank/sh/export.sh
+++ nel/tools/3d/build_gamedata/processes/clodbank/sh/export.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# *** Export shape files (.shape) from Max
+
+# *** Export character lod shape files (.clod) from Max
 
 exec_timeout='../../bin/exec_timeout.exe'
 
@@ -17,34 +18,29 @@ database_directory=`cat ../../cfg/site.cfg | grep "database_directory" | sed -e 
 build_gamedata_directory=`cat ../../cfg/site.cfg | grep "build_gamedata_directory" | sed -e 's/build_gamedata_directory//' | sed -e 's/ //g' | sed -e 's/=//g'`
 
 # Get the shape directories
-shape_source_directories=`cat ../../cfg/directories.cfg | grep "shape_source_directory" | sed -e 's/shape_source_directory//' | sed -e 's/ //g' | sed -e 's/=//g'`
-
-# Get the options
-seoel=`cat ../../cfg/config.cfg | grep "shape_export_opt_export_lighting" | sed -e 's/shape_export_opt_export_lighting//' | sed -e 's/ //g' | sed -e 's/=//g'`
-seos=`cat ../../cfg/config.cfg | grep "shape_export_opt_shadow" | sed -e 's/shape_export_opt_shadow//' | sed -e 's/ //g' | sed -e 's/=//g'`
-seoll=`cat ../../cfg/config.cfg | grep "shape_export_opt_lighting_limit" | sed -e 's/shape_export_opt_lighting_limit//' | sed -e 's/ //g' | sed -e 's/=//g'`
-seols=`cat ../../cfg/config.cfg | grep "shape_export_opt_lumel_size" | sed -e 's/shape_export_opt_lumel_size//' | sed -e 's/ //g' | sed -e 's/=//g'`
-seoo=`cat ../../cfg/config.cfg | grep "shape_export_opt_oversampling" | sed -e 's/shape_export_opt_oversampling//' | sed -e 's/ //g' | sed -e 's/=//g'`
+clod_source_directories=`cat ../../cfg/directories.cfg | grep "clod_source_directory" | sed -e 's/clod_source_directory//' | sed -e 's/ //g' | sed -e 's/=//g'`
 
 # Log error
 echo ------- > log.log
-echo --- Export shape >> log.log
+echo --- Export clod >> log.log
 echo ------- >> log.log
 echo ------- 
-echo --- Export shape 
+echo --- Export clod
 echo ------- 
 date >> log.log
 date
 
 # For each directoy
 
-for i in $shape_source_directories ; do
+for i in $clod_source_directories ; do
 	# Copy the script
-	cat maxscript/shape_export.ms | sed -e "s&shape_source_directory&$database_directory/$i&g" | sed -e "s&output_directory_without_coarse_mesh&$build_gamedata_directory/processes/shape/shape&g" | sed -e "s&output_directory_with_coarse_mesh&$build_gamedata_directory/processes/shape/shape_with_coarse_mesh&g" | sed -e "s&shape_export_opt_export_lighting&$seoel&g" | sed -e "s&shape_export_opt_shadow&$seos&g" | sed -e "s&shape_export_opt_lighting_limit&$seoll&g" | sed -e "s&shape_export_opt_lumel_size&$seols&g" | sed -e "s&shape_export_opt_oversampling&$seoo&g" | sed -e "s&shape_lightmap_path&$build_gamedata_directory/processes/shape/lightmap&g" > $max_directory/scripts/shape_export.ms
+	cat maxscript/clod_export.ms | sed -e "s&shape_source_directory&$database_directory/$i&g" | sed -e "s&output_directory&$build_gamedata_directory/processes/clodbank/clod&g" > $max_directory/scripts/clod_export.ms
 
 	# Start max
-	$exec_timeout $timeout $max_directory/3dsmax.exe -U MAXScript shape_export.ms -q -mi -vn
+	$exec_timeout $timeout $max_directory/3dsmax.exe -U MAXScript clod_export.ms -q -mi -vn
 
 	# Concat log.log files
 	cat $max_directory/log.log >> log.log
 done
+
+

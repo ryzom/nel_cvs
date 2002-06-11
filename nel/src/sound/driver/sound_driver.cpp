135057af276e63f3e400c80170fd56c1f35cb360
hanappe <hanappe@nevrax.com>
2002-06-11 11:37:21 +0200

    Incremented version number

--- nel/src/sound/driver/sound_driver.cpp
+++ nel/src/sound/driver/sound_driver.cpp
@@ -1,7 +1,7 @@
 /** \file sound_driver.cpp
  * ISoundDriver: sound driver interface
  *
- * $Id: sound_driver.cpp,v 1.5 2001/09/03 14:21:31 cado Exp $
+ * $Id: sound_driver.cpp,v 1.6 2002/06/11 09:37:21 hanappe Exp $
  */
 
 /* Copyright, 2001 Nevrax Ltd.
@@ -43,7 +43,7 @@ namespace NLSOUND
 
 
 // Interface version
-const uint32 ISoundDriver::InterfaceVersion = 0x02;
+const uint32 ISoundDriver::InterfaceVersion = 0x03;
 
 typedef ISoundDriver* (*ISDRV_CREATE_PROC)(void); 
 const char *IDRV_CREATE_PROC_NAME = "NLSOUND_createISoundDriverInstance";

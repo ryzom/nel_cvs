5def52ddd57a013de30ca0464211072abf662466
Matthieu Besson <besson@nevrax.com>
2002-06-26 12:11:29 +0200

    ADDED: multiple directories export

--- nel/tools/3d/build_gamedata/processes/interface/sh/build.sh
+++ nel/tools/3d/build_gamedata/processes/interface/sh/build.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 
-../../bin/build_interface tga_tmp tga/texture_interface_v2.tga
+
+# *** Export interface tile (.tga)
+
+# Get the database directory
+database_directory=`cat ../../cfg/site.cfg | grep "database_directory" | sed -e 's/database_directory//g' | sed -e 's/ //g' | sed -e 's/=//g'`
+
+# Get the interface directories
+interface_source_directories=`cat ../../cfg/directories.cfg | grep "interface_source_directories" | sed -e 's/interface_source_directories//' | sed -e 's/ //g' | sed -e 's/=//g'`
+
+# Log error
+echo ------- > log.log
+echo --- Export interface >> log.log
+echo ------- >> log.log
+echo ------- 
+echo --- Export interface 
+echo ------- 
+date >> log.log
+date
+
+# For each interface directory
+for i in $interface_source_directories ; do
+	# Copy
+	niouname=`echo $i | sed -e 's&/&_&g'`
+	rm tga_tmp/*.tga
+	cp -u -p $database_directory/$i/*.tga tga_tmp 2>> log.log
+	../../bin/build_interface tga_tmp tga/texture_$niouname.tga
+done
+
+
--- nel/tools/3d/build_gamedata/processes/interface/sh/export.sh
+++ nel/tools/3d/build_gamedata/processes/interface/sh/export.sh
@@ -1,25 +1,3 @@
 #!/bin/bash
 
 # *** Export interface tile (.tga)
-
-# Get the database directory
-database_directory=`cat ../../cfg/site.cfg | grep "database_directory" | sed -e 's/database_directory//g' | sed -e 's/ //g' | sed -e 's/=//g'`
-
-# Get the interface directories
-interface_source_directories=`cat ../../cfg/directories.cfg | grep "interface_source_directories" | sed -e 's/interface_source_directories//' | sed -e 's/ //g' | sed -e 's/=//g'`
-
-# Log error
-echo ------- > log.log
-echo --- Export interface >> log.log
-echo ------- >> log.log
-echo ------- 
-echo --- Export interface 
-echo ------- 
-date >> log.log
-date
-
-# For each interface directory
-for i in $interface_source_directories ; do
-	# Copy
-	cp -u -p $database_directory/$i/*.tga tga_tmp 2>> log.log
-done

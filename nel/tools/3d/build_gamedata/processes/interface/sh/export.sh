3e6dfb0baf344b83f2c15f72317fd9b8f52fcb62
Matthieu Besson <besson@nevrax.com>
2002-06-28 17:57:39 +0200

    ADDED: copy fullscreen images

--- nel/tools/3d/build_gamedata/processes/interface/sh/export.sh
+++ nel/tools/3d/build_gamedata/processes/interface/sh/export.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
 # *** Export interface tile (.tga)
+
+# Get the database directory
+database_directory=`cat ../../cfg/site.cfg | grep "database_directory" | sed -e 's/database_directory//g' | sed -e 's/ //g' | sed -e 's/=//g'`
+
+# Get the interface directories
+interface_fullscreen_directories=`cat ../../cfg/directories.cfg | grep "interface_fullscreen_directories" | sed -e 's/interface_fullscreen_directories//' | sed -e 's/ //g' | sed -e 's/=//g'`
+
+# Log error
+echo ------- > log.log
+echo --- Export interface >> log.log
+echo ------- >> log.log
+echo ------- 
+echo --- Export interface 
+echo ------- 
+date >> log.log
+date
+
+# For each interface directory
+for i in $interface_fullscreen_directories; do
+	# Copy
+	cp -u -p $database_directory/$i/*.tga tga 2>> log.log
+done

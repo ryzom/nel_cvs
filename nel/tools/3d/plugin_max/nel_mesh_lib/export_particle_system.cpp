5d3e12a250453deebc8fef91419b6c47307f0d76
Nicolas Vizerie <vizerie@nevrax.com>
2002-07-03 11:16:44 +0200

    #CHANGE: export of particle system in a separate file

---
+++ nel/tools/3d/plugin_max/nel_mesh_lib/export_particle_system.cpp
@@ -0,0 +1,78 @@
+/** \file export_particle_system.cpp
+ *
+ * $Id: export_particle_system.cpp,v 1.1 2002/07/03 09:16:44 vizerie Exp $
+ */
+
+/* Copyright, 2000, 2001, 2002 Nevrax Ltd.
+ *
+ * This file is part of NEVRAX NEL.
+ * NEVRAX NEL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+
+ * NEVRAX NEL is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with NEVRAX NEL; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA.
+ */
+
+#include "StdAfx.h"
+#include <3d/particle_system_model.h>
+#include <3d/particle_system_shape.h>
+#include "export_lod.h"
+#include "export_nel.h"
+
+
+#define NEL_OBJET_NAME_DATA 1970
+
+using namespace NL3D;
+
+IShape* CExportNel::buildParticleSystem(INode& node, TimeValue time)
+{
+	Object *obj = node.EvalWorldState(time).obj;
+	nlassert(obj);
+	// build the shape from the file name
+ 	AppDataChunk *ad = obj->GetAppDataChunk(MAXSCRIPT_UTILITY_CLASS_ID, UTILITY_CLASS_ID, NEL_OBJET_NAME_DATA );
+	if (ad&&ad->data)
+	{											
+		NL3D::CShapeStream ss;
+		NLMISC::CIFile iF;
+		if (iF.open((const char *) ad->data))
+		{
+			iF.serial(ss);
+
+			CParticleSystemShape *pss = NLMISC::safe_cast<CParticleSystemShape *>(ss.getShapePointer());
+
+			// ********************************
+			// *** Export default transformation
+			// ********************************
+
+			// Get the node matrix
+			Matrix3 localTM;
+			getLocalMatrix (localTM, node, time);
+
+			// Get the translation, rotation, scale of the node
+			CVector pos, scale;
+			CQuat rot;
+			decompMatrix (scale, rot, pos, localTM);
+
+			// Set the default values
+			pss->getDefaultPos()->setValue(pos);					
+			pss->getDefaultScale()->setValue(scale);					
+			pss->getDefaultRotQuat()->setValue(rot);												
+			return pss;
+		}
+		else
+		{
+			mprintf("Error : Can't find %s while exporting a particle system \n", (const char *) ad->data);
+			return NULL;
+		}				
+	}
+	return NULL;
+}

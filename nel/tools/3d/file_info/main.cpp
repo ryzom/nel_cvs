c06b508a75adac0c6a16d8dbdee87c6e60fcd2c4
Lionel Bérenguier <berenguier@nevrax.com>
2002-09-05 14:12:42 +0200

    ADDED: .shape, -ms

--- nel/tools/3d/file_info/main.cpp
+++ nel/tools/3d/file_info/main.cpp
@@ -1,7 +1,7 @@
 /** \file main.cpp
  * Display info on many NEL files. ig, zone etc...
  *
- * $Id: main.cpp,v 1.2 2002/05/28 08:35:47 berenguier Exp $
+ * $Id: main.cpp,v 1.3 2002/09/05 12:12:42 berenguier Exp $
  */
 
 /* Copyright, 2000 Nevrax Ltd.
@@ -31,6 +31,10 @@
 #include "3d/skeleton_shape.h"
 #include "3d/register_3d.h"
 #include "nel/misc/file.h"
+#include "nel/misc/path.h"
+#include "3d/mesh.h"
+#include "3d/mesh_mrm.h"
+#include "3d/mesh_multi_lod.h"
 
 
 using	namespace std;
@@ -38,95 +42,229 @@ using	namespace NLMISC;
 using	namespace NL3D;
 
 
+// ***************************************************************************
+void	displayGeom(FILE *logStream, const CMeshGeom &geom)
+{
+	uint	i,j;
+	uint	numFaces=0;
+	for(i=0;i<geom.getNbMatrixBlock();i++)
+	{
+		for(j=0;j<geom.getNbRdrPass(i);j++)
+		{
+			numFaces+= geom.getRdrPassPrimitiveBlock(i,j).getNumTri();
+		}
+	}
+	fprintf(logStream, "  Standard Mesh %s\n", geom.isSkinned()?"Skinned":"" );
+	fprintf(logStream, "  NumFaces: %d\n", numFaces );
+	fprintf(logStream, "  NumVertices: %d\n", geom.getVertexBuffer().getNumVertices() );
+}
+
+void	displayMRMGeom(FILE *logStream, const CMeshMRMGeom &geom)
+{
+	uint	i,j;
+	uint	numFaces=0;
+	uint	numFacesLodMax=0;
+	for(i=0;i<geom.getNbLod();i++)
+	{
+		for(j=0;j<geom.getNbRdrPass(i);j++)
+		{
+			uint	nPassFaces= geom.getRdrPassPrimitiveBlock(i,j).getNumTri();
+			numFaces+= nPassFaces;
+			if(i==geom.getNbLod()-1)
+				numFacesLodMax+= nPassFaces;
+		}
+	}
+	fprintf(logStream, "  MRM Mesh %s\n", geom.isSkinned()?"Skinned":"" );
+	fprintf(logStream, "  NumFaces(Max Lod): %d\n", numFacesLodMax );
+	fprintf(logStream, "  NumFaces(Sum all Lods): %d\n", numFaces );
+	fprintf(logStream, "  NumVertices(Sum all Lods): %d\n", geom.getVertexBuffer().getNumVertices() );
+}
+
+
+// ***************************************************************************
 /// Dispaly info for file in stdout
-void	displayInfo(FILE *logStream, const char *fileName)
+void	displayInfoFileInStream(FILE *logStream, const char *fileName, const char *option)
 {
 	if(fileName==NULL)
 		return;
 
-	// some general info.
-	fprintf(logStream, "File: %s\n", fileName);
-	fprintf(logStream, "***********\n\n");
-
-	if(strstr(fileName, ".zone"))
+	// Special option.
+	if( string(option)=="-ms" )
 	{
-		// read the zone.
-		CIFile	file(fileName);
-		CZone	zone;
-		file.serial(zone);
-
-		// retreive info on Zone
-		CZoneInfo	zoneInfo;
-		zone.retrieve(zoneInfo);
-
-		// display Info on the zone:
-		fprintf(logStream, "  Num Patchs: %d\n", zone.getNumPatchs() );
-		fprintf(logStream, "  Num PointLights: %d\n", zoneInfo.PointLights.size() );
-	}
-	else if(strstr(fileName, ".ig"))
-	{
-		// read the ig.
-		CIFile	file(fileName);
-		CInstanceGroup	ig;
-		file.serial(ig);
-
-		// display Info on the ig:
-		fprintf(logStream, "  Num Instances: %d\n", ig.getNumInstance() );
-		fprintf(logStream, "  Num PointLights: %d\n", ig.getPointLightList().size() );
-	}
-	else if(strstr(fileName, ".skel"))
-	{
-		// read the skeleton.
-		CIFile	file(fileName);
-		CShapeStream	shapeStream;
-		file.serial(shapeStream);
-		CSkeletonShape	*skel= dynamic_cast<CSkeletonShape*>(shapeStream.getShapePointer());
-
-		if(skel)
+		if(strstr(fileName, ".shape"))
 		{
-			vector<CBoneBase>	bones;
-			skel->retrieve(bones);
-			// Display Bone Infos.
-			fprintf(logStream, "Num Bones: %d\n", bones.size());
-			for(uint i=0; i<bones.size(); i++)
+			// read the skeleton.
+			CIFile	file(fileName);
+			CShapeStream	shapeStream;
+			file.serial(shapeStream);
+
+			// Test Type
+			CMesh			*mesh= dynamic_cast<CMesh*>(shapeStream.getShapePointer());
+
+			// Mesh ??
+			if( mesh )
 			{
-				// get default pos.
-				const CAnimatedValueBlendable<CVector>	&posValue= 
-					static_cast<const CAnimatedValueBlendable<CVector>	&>(bones[i].DefaultPos.getValue());
-				CVector	pos= posValue.Value;
-
-				// get default rotquat.
-				const CAnimatedValueBlendable<CQuat>	&rotValue= 
-					static_cast<const CAnimatedValueBlendable<CQuat>	&>(bones[i].DefaultRotQuat.getValue());
-				CQuat	rotQuat= rotValue.Value;
-
-				// get default scale.
-				const CAnimatedValueBlendable<CVector>	&scaleValue= 
-					static_cast<const CAnimatedValueBlendable<CVector>	&>(bones[i].DefaultScale.getValue());
-				CVector	scale= scaleValue.Value;
-
-				// print info
-				fprintf(logStream, "Bone %2d. %s.\n", i, bones[i].Name.c_str());
-				fprintf(logStream, "   Pos:      (%2.3f, %2.3f, %2.3f)\n", 
-					pos.x, pos.y, pos.z);
-				fprintf(logStream, "   RotQuat:  (%2.3f, %2.3f, %2.3f, %2.3f)\n", 
-					rotQuat.x, rotQuat.y, rotQuat.z, rotQuat.w);
-				fprintf(logStream, "   Scale:    (%2.3f, %2.3f, %2.3f)\n",
-					scale.x, scale.y, scale.z);
+				if( mesh->getMeshGeom().isSkinned() )
+				{
+					fprintf(logStream, "%s is Skinned, but without MRM!!!\n", fileName);
+				}
 			}
+
+			// release
+			delete shapeStream.getShapePointer();
+			shapeStream.setShapePointer(NULL);
+		}
+	}
+	// Std Way.
+	else
+	{
+		// some general info.
+		fprintf(logStream, "File: %s\n", fileName);
+		fprintf(logStream, "***********\n\n");
+
+		if(strstr(fileName, ".zone"))
+		{
+			// read the zone.
+			CIFile	file(fileName);
+			CZone	zone;
+			file.serial(zone);
+
+			// retreive info on Zone
+			CZoneInfo	zoneInfo;
+			zone.retrieve(zoneInfo);
+
+			// display Info on the zone:
+			fprintf(logStream, "  Num Patchs: %d\n", zone.getNumPatchs() );
+			fprintf(logStream, "  Num PointLights: %d\n", zoneInfo.PointLights.size() );
+		}
+		else if(strstr(fileName, ".ig"))
+		{
+			// read the ig.
+			CIFile	file(fileName);
+			CInstanceGroup	ig;
+			file.serial(ig);
+
+			// display Info on the ig:
+			fprintf(logStream, "  Num Instances: %d\n", ig.getNumInstance() );
+			fprintf(logStream, "  Num PointLights: %d\n", ig.getPointLightList().size() );
+		}
+		else if(strstr(fileName, ".skel"))
+		{
+			// read the skeleton.
+			CIFile	file(fileName);
+			CShapeStream	shapeStream;
+			file.serial(shapeStream);
+			CSkeletonShape	*skel= dynamic_cast<CSkeletonShape*>(shapeStream.getShapePointer());
+
+			if(skel)
+			{
+				vector<CBoneBase>	bones;
+				skel->retrieve(bones);
+				// Display Bone Infos.
+				fprintf(logStream, "Num Bones: %d\n", bones.size());
+				for(uint i=0; i<bones.size(); i++)
+				{
+					// get default pos.
+					const CAnimatedValueBlendable<CVector>	&posValue= 
+						static_cast<const CAnimatedValueBlendable<CVector>	&>(bones[i].DefaultPos.getValue());
+					CVector	pos= posValue.Value;
+
+					// get default rotquat.
+					const CAnimatedValueBlendable<CQuat>	&rotValue= 
+						static_cast<const CAnimatedValueBlendable<CQuat>	&>(bones[i].DefaultRotQuat.getValue());
+					CQuat	rotQuat= rotValue.Value;
+
+					// get default scale.
+					const CAnimatedValueBlendable<CVector>	&scaleValue= 
+						static_cast<const CAnimatedValueBlendable<CVector>	&>(bones[i].DefaultScale.getValue());
+					CVector	scale= scaleValue.Value;
+
+					// print info
+					fprintf(logStream, "Bone %2d. %s.\n", i, bones[i].Name.c_str());
+					fprintf(logStream, "   Pos:      (%2.3f, %2.3f, %2.3f)\n", 
+						pos.x, pos.y, pos.z);
+					fprintf(logStream, "   RotQuat:  (%2.3f, %2.3f, %2.3f, %2.3f)\n", 
+						rotQuat.x, rotQuat.y, rotQuat.z, rotQuat.w);
+					fprintf(logStream, "   Scale:    (%2.3f, %2.3f, %2.3f)\n",
+						scale.x, scale.y, scale.z);
+				}
+			}
+			else
+			{
+				fprintf(logStream, "Bad Skel file\n");
+			}
+
+			// release
+			delete shapeStream.getShapePointer();
+			shapeStream.setShapePointer(NULL);
+		}
+		else if(strstr(fileName, ".shape"))
+		{
+			// read the shape.
+			CIFile	file(fileName);
+			CShapeStream	shapeStream;
+			file.serial(shapeStream);
+
+			// Test Type
+			CMesh			*mesh= dynamic_cast<CMesh*>(shapeStream.getShapePointer());
+			CMeshMRM		*meshMRM= dynamic_cast<CMeshMRM*>(shapeStream.getShapePointer());
+			CMeshMultiLod	*meshMulti= dynamic_cast<CMeshMultiLod*>(shapeStream.getShapePointer());
+
+			// Mesh ??
+			if( mesh )
+			{
+				displayGeom(logStream, mesh->getMeshGeom());
+			}
+			// MRM ??
+			else if( meshMRM )
+			{
+				displayMRMGeom(logStream, meshMRM->getMeshGeom());
+			}
+			// MultiLod??
+			else if( meshMulti )
+			{
+				uint	numSlots= meshMulti->getNumSlotMesh ();
+				fprintf(logStream, "  Num Lods: %d\n", numSlots );
+				if(numSlots)
+				{
+					const CMeshGeom		*meshGeom= dynamic_cast<const CMeshGeom*>(&(meshMulti->getMeshGeom(0)));
+					const CMeshMRMGeom	*meshMRMGeom= dynamic_cast<const CMeshMRMGeom*>(&(meshMulti->getMeshGeom(0)));
+					if( meshGeom )
+						displayGeom(logStream, *meshGeom);
+					else if( meshMRMGeom )
+						displayMRMGeom(logStream, *meshMRMGeom);
+				}
+			}
+			else
+			{
+				fprintf(logStream, "Unsupported .shape type for display info\n");
+			}
+
+			// release
+			delete shapeStream.getShapePointer();
+			shapeStream.setShapePointer(NULL);
 		}
 		else
 		{
-			fprintf(logStream, "Bad Skel file\n");
+			fprintf(logStream, "unsupported format\n");
 		}
-	}
-	else
-	{
-		fprintf(logStream, "unsupported format\n");
 	}
 }
 
 
+// ***************************************************************************
+// dispaly info for a file.
+void		displayInfoFile(FILE *logStream, const char *fileName, const char *option)
+{
+	// Display on screen.
+	displayInfoFileInStream(stdout, fileName,option);
+	// Display in log
+	if(logStream)
+		displayInfoFileInStream(logStream, fileName,option);
+}
+
+
+// ***************************************************************************
 /// Dispaly info cmd line
 int		main(int argc, const char *argv[])
 {
@@ -134,27 +272,55 @@ int		main(int argc, const char *argv[])
 
 	if(argc<2)
 	{
-		puts("Usage: ig_info file.???");
-		puts("    For now, only .ig, .zone, .skel are supported");
+		puts("Usage: ig_info file.??? [opt]");
+		puts("Usage: ig_info directory [opt]");
+		puts("    For now, only .ig, .zone, .skel, .shape are supported");
 		puts("    Results are displayed too in \"c:/temp/file_info.log\" ");
+		puts("    [opt] can get: ");
+		puts("    -ms display only a Warning if file is a .shape and is a Mesh, skinned, but without MRM");
 		puts("Press any key");
 		_getch();
 		return -1;
 	}
 
-	const char *fileName= argv[1];
 
-	// Display on screen.
-	displayInfo(stdout, fileName);
+	// Parse option.
+	const	char	*option= "";
+	if(argc>=3)
+	{
+		option= argv[2];
+	}
 
-	// Display in file.
+
+	// Open log
 	FILE	*logStream;
 	logStream= fopen("C:/temp/file_info.log", "wt");
-	if(logStream)
+
+
+	// parse dir or file ??
+	const char *fileName= argv[1];
+	if(CFile::isDirectory(fileName))
 	{
-		displayInfo(logStream, fileName);
-		fclose(logStream);
+		// dir all files.
+		std::vector<std::string>	listFile;
+		CPath::getPathContent (fileName, false, false, true, listFile);
+
+		// For all files.
+		for(uint i=0;i<listFile.size();i++)
+		{
+			displayInfoFile(logStream, listFile[i].c_str(), option);
+		}
 	}
+	else
+	{
+		displayInfoFile(logStream, fileName, option);
+	}
+
+
+	// close log
+	if(logStream)
+		fclose(logStream);
+
 
 	puts("Press any key");
 	_getch();

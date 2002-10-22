3ec7e7d2ef2d77204e350ec6da22291b93652c0f
Vianney Lecroart <lecroart@nevrax.com>
2002-10-22 10:41:44 +0200

    ADDED: permissions

--- nelns/login_system/database/nel.sql
+++ nelns/login_system/database/nel.sql
@@ -4,18 +4,34 @@
 #--------------------------------------------------------
 # Server version 3.23.34
 
+USE nel;
+
+
+#
+# Table structure for table 'permission'
+#
+
+DROP TABLE IF EXISTS permission;
+CREATE TABLE `permission` (
+  `UId` int(10) unsigned NOT NULL default '0',
+  `ClientApplication` char(64) NOT NULL default 'snowballs'
+) TYPE=MyISAM;
+
+
 
 #
 # Table structure for table 'shard'
 #
 
+DROP TABLE IF EXISTS shard;
 CREATE TABLE `shard` (
   `ShardId` int(10) NOT NULL auto_increment,
   `WsAddr` varchar(64) default NULL,
   `NbPlayers` int(10) unsigned default '0',
-  `ShardName` varchar(64) default NULL,
+  `Name` varchar(64) default NULL,
   `Online` tinyint(1) unsigned default '0',
-  `ProgramName` varchar(64) NOT NULL default '',
+  `ClientApplication` varchar(64) NOT NULL default '',
+  `Version` varchar(64) default NULL,
   PRIMARY KEY  (`ShardId`)
 ) TYPE=MyISAM COMMENT='contains all shards informations for login system';
 
@@ -25,6 +41,7 @@ CREATE TABLE `shard` (
 # Table structure for table 'user'
 #
 
+DROP TABLE IF EXISTS user;
 CREATE TABLE `user` (
   `UId` int(10) NOT NULL auto_increment,
   `Login` varchar(64) NOT NULL default '',

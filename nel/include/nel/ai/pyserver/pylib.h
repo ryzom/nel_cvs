9cb70352d6fb7047c5669fe5936eac0666d99c78
Sameh Chafik <chafik@nevrax.com>
2003-01-20 11:56:41 +0100

    #added: comment on pylib.h

--- nel/include/nel/ai/pyserver/pylib.h
+++ nel/include/nel/ai/pyserver/pylib.h
@@ -1,5 +1,29 @@
-#ifndef _GAME_PYTHON_LIB_H_
-#define _GAME_PYTHON_LIB_H_
+#ifndef GAME_PYTHON_LIB_H
+#define GAME_PYTHON_LIB_H
+/** \file pylib.h
+ * class to run and manage python script
+ *
+ * $Id: pylib.h,v 1.10 2003/01/20 10:56:41 chafik Exp $
+ */
+
+/* Copyright, 2000 Nevrax Ltd.
+ *
+ * This file is part of NEVRAX NEL.
+ * NEVRAX NEL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+
+ * NEVRAX NEL is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with NEVRAX NEL; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA.
+ */
 
 #include <Python.h>
 #include "nel/ai/pyserver/pythonsupport.h"
@@ -37,53 +61,47 @@ namespace NLAIPYSERVER
 			_DebugMode = false;			
 		}
 
-	//!Met le Mode debug à ON
+		///Put the debug mode on
 		bool	setDebug()
 		{
 			return (_DebugMode = true); 
 		}
 
+		///return the debug mode status
 		sint32	getDebugStat()
 		{
 			return _DebugMode;
 		}
 
-	//!Met le Mode debug à OFF 
+		///Put the debug mode off
 		bool	unSetDebug()
 		{
 			return (_DebugMode = false);
 		}
 
 
-	//!Check the debug mode state.
+		///test debug mode is on
 		bool isDebug()
 		{
 			return (_DebugMode == true);
 		}
 
-	/**	
-		Execution d'instruction Python dans le mode (Debug/Release) on fonction de l'etat de _DebugMode
+		/**				
+			Run Python script on debug/release mode depend on _DebugMode status.
 
-		Code:				est le texte contenant les instruction Python à executer. 
-							Attention generalement les editeurs windows produit des fin 
-							de ligne marquer par les caractéres \n\r, l'interpréteur Python 
-							n'aime pas du tout le caractaire \r en fin de ligne donc 
-							il faut filtrer le texte à executer avant l'appelle à cette procedure,
-							c'est à dire convertire tout \r en \n. Ou utiliser la methode GetCodeFromFile.
+			\param Code:		Python script on text mode, warrning, there are difference between UNIX & Window, on UNIX system 
+								there are a \n\r to mark the end of line, and Python parser don't like the \r syntax, use the methode GetCodeFromFile
+								to clear all \r.								
 
-		NameSpace:			Le name space du code à executer. NULL c'est le name space Python par
-							defaut c'est à dire __main__.
+			\param NameSpace:	Name space od code to execute. NULL is the default Python name space that is __main__.
 
-		ModeExec:			Le type d'excecution. Ce type peut être par defaut execution normal (file_input),
-							ou evalution d'expression (eval_input).
+			\param ModeExec:	execution type file_input or eval_input.
 
-		Format:			Le type de parametre dans le cas ou l'execution se fait avec eval_input.
-							ce type peut être	"O" (Objet);
-												"i" (intger) ........ ; voir la doc Python.
-							
-		ResultTarget:	Pointeur sur un pointeur pour resevoir le resultat d'evaluation.
+			\param Format:		Argument format, for exempel that can bee: "O" (as Object), "i" (intger) ........ see python manual.
+								
+			\param ResultTarget:	The return of the result on eval_input mode.
 \code
-Exemple:
+Sample:
 	pExePythonExpression->RunCode(	"class C():\n"
 					"\t def __init__(self):\n"
 					"\t\t self.x = 5\n"
@@ -92,106 +110,89 @@ Exemple:
 					"\t\t return self.x*nX,self.y*nY\n");
 	PyObject *PyObjetA;
 	pExePythonExpression->RunCode("A = C()",NULL,eval_input,"O",&PyObjetA);
-\endcode
+\endcode								
 
-							avec eval_input PyObjetA est le pointeur sur l'instance de l'Objet Python A crée dans Python
+		  the method return	0 if Ok; -1  if fail.
 
-	  return:
-			0	Ok;
-			-1  ehec.
-
-	*/
+		*/
 		sint32 runCode(const char  *Code,char  *NameSpace = NULL,sint32 ModeExec = Py_file_input,char *Format = NULL,void *ResultTarget = NULL,sint32 Error = true );	
 	
 	/**
-		Execution d'une methode membre d'un objet Python dans le mode (Debug/Release) on fonction de l'etat de _DebugMode
+		Run a python method on a python class.		
 
-		Obj:				pointeur sur une instance d'un Objet python (voir l'exemple plus haut). 
+		\param Obj:			Pointer of the module that contain method, see exemple on runCode or python manual. 
 
-		MethodName:		Le nom de la methode membre à executer (je rappelle qu'en python tout est public).
+		\param MethodName:	Name of metod to run.
 
-		ResFormat:		Type du resultat attendu.
+		\param ResFormat:	Type of expected result.
 
-		ResTarget:		pointeur pour resevoir le resultat de l'appelle de la methode.
+		\param ResTarget:		The return of the result.
 							
-		Format:			Le type de l'argument par exemple: ResFormat "(iiO)" (2 sint32 et un pointeur sur un objet python). 						
-							viennent ensuite se déposer sur la pile les valeurs de l'argument.
+		\param Format:		Argument format, for exempel that can bee: "O" (as Object), "i" (intger) ........ see python manual.
 \code
-exemple:
+Sample:
 	PyObject *PyObjetA,*PyObjetResult;
 	pExePythonExpression->RunCode("A = C()",NULL,eval_input,"O",&PyObjetA);
 	pExePythonExpression->RunMethod(PyObjetA,"M","O",PyObjetResult,"ii",5,16);
 \endcode
 
-							Le resulta sera un tupule Python du type(25,400);
-
-		return:
-			0	Ok;
-			-1  ehec.
-
-		
-
+		The result is a Python tupule as (25,400);		
+		The method return	0 if Ok; -1  if fail.
 	*/		
 
 		sint32 runMethod(PyObject *Obj,char  *MethodName,char *ResFormat, void *ResTarget,char *Format,...);
-	
-
+		
 	/**
-		Execution d'une fonctio membre d'un module Python dans le mode (Debug/Release) on fonction de l'etat de _DebugMode
+		Run a python function on an module.				
 
-		ModuleName:		Nom du module contenant la fonction à executer.
+		\param ModuleName:		name of the module.
 
-		MethodName:		Le nom de la fonction à executer.
+		\param MethodName:		name of method to run.
 
-		ResFormat:		Type du resultat attendu.
+		\param ResFormat:	Type of expected result.
 
-		ResTarget:		pointeur pour resevoir le resultat de l'appelle de la fonction.
+		\param ResTarget:		The return of the result.
 							
-		Format:			Le type de l'argument par exemple: ResFormat "(iiO)" (2 sint32 et un pointeur sur un objet python). 						
-							viennent ensuite se déposer sur la pile les valeurs de l'argument.
-		return:
-			0	Ok;
-			-1  ehec.
+		\param Format:		Argument format, for exempel that can bee: "O" (as Object), "i" (intger) ........ see python manual.
+
+		The method return	0 if Ok; -1  if fail.
 	*/
 		sint32 runFunction(char *ModuleName,char  *MethodName,char *ResFormat, void *ResTarget,char *Format,...);
 
 	
 	/**	
-		Obtenir la valeur d'un attribut dans un objet Python.
+		get valu of an attribut in an python object.
 
-		Obj:				pointeur sur une instance d'un Objet python (voir l'exemple plus haut).
+		\param Obj:		python object.
 
-		AttribName:		Le nom de l'attribut à obtenir.
+		\param AttribName: name of attribute to eval.
 
-		ResFormat:		Type du resultat attendu.		
+		\param ResFormat:	Type of expected result.
 
-		ResTarget:		pointeur pour resevoir le resultat de l'appelle de la fonction.
+		\param ResTarget:		The return of the result.
 							
 \code
-exemple:
+Sample:
 
-	sint32 PyLeXDeA;
-	sint32 PyLeYDeA;
-	pExePythonExpression->GetMembreValue(PyObjetA,"X","i",&PyLeXDeA);
-	pExePythonExpression->GetMembreValue(PyObjetA,"Y","i",&PyLeYDeA);
+	sint32 PyTheXOfA;
+	sint32 PyTheYOfA;
+	pExePythonExpression->GetMembreValue(PyObjetA,"X","i",&PyTheXOfA);
+	pExePythonExpression->GetMembreValue(PyObjetA,"Y","i",&PyTheYOfA);
 \endcode
 
-		return:
-			0	Ok;
-			-1  ehec.
+		The method return	0 if Ok; -1  if fail.
 	*/
 		sint32	getMembreValue(PyObject *Obj,char  *AttribName,char *ResFormat, void *ResTarget);	
 		
 		
 	/**
-		Changer la valeur d'un attribut dans un objet Python.
+		Change attribut value of an Python object.
 
-		Obj:				pointeur sur une instance d'un Objet python (voir l'exemple plus haut).
+		\param Obj:		python object.
 
-		AttribName:		Le nom de l'attribut à obtenir.
+		\param AttribName: name of attribute to eval.
 
-		ResFormat:		Type du resultat attendu.
-							vient ensuite la valeur de l'attribut.
+		\param ResTarget: The return of the result.
 
 \code			
 exemple:							
@@ -200,10 +201,7 @@ exemple:
 	pExePythonExpression->RunMethod(PyObjetA,"M","O",PyObjetResult,"ii",5,16);
 \endcode
 
-							Le resulta sera un tupule Python du type(100,30);
-		return:
-			0	Ok;
-			-1  ehec.
+		The method return	0 if Ok; -1  if fail.
 	*/
 		sint32	setMembreValue(PyObject *Obj,char  *AttribName,char *ResFormat, ...);	
 		

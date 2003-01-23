9d117b05025c8e7575d66d7b3484f5f83e2e6c8b
Lionel Bérenguier <berenguier@nevrax.com>
2003-01-23 11:14:12 +0100

    FIXED: new build_interface usage ADDED: "DXTC" directories grouping into one TGA (for future V3 interfaces)

--- nel/tools/3d/build_gamedata/processes/interface/sh/build.sh
+++ nel/tools/3d/build_gamedata/processes/interface/sh/build.sh
@@ -10,6 +10,9 @@ database_directory=`cat ../../cfg/site.cfg | grep "database_directory" | sed -e 
 # Get the interface directories
 interface_source_directories=`cat ../../cfg/directories.cfg | grep "interface_source_directories" | sed -e 's/interface_source_directories//' | sed -e 's/ //g' | sed -e 's/=//g'`
 
+# Get the interface directories to compress in one DXTC only
+interface_source_dxtc_directories=`cat ../../cfg/directories.cfg | grep "interface_source_dxtc_directories" | sed -e 's/interface_source_dxtc_directories//' | sed -e 's/ //g' | sed -e 's/=//g'`
+
 # Log error
 echo ------- > log.log
 echo --- Export interface >> log.log
@@ -26,10 +29,25 @@ for i in $interface_source_directories ; do
 	niouname=`echo $i | sed -e 's&/&_&g'`
 	rm tga_tmp/*.[tT][gG][aA]
 	cp -u -p $database_directory/$i/*.[tT][gG][aA] tga_tmp 2>> log.log
-	../../bin/build_interface tga_tmp tga/texture_$niouname.tga
+	../../bin/build_interface tga/texture_$niouname.tga tga_tmp
 
 	# Idle
 	../../idle.bat
 done
+
+
+# For each interface directory to compress in one DXTC
+rm tga_tmp/*.[tT][gG][aA]
+for i in $interface_source_dxtc_directories ; do
+	# Copy
+	cp -u -p $database_directory/$i/*.[tT][gG][aA] tga_tmp 2>> log.log
+done
+
+# build all files in tga_tmp into one tga. Let the OpenGL compress it at runTime (better result for some important cases)
+../../bin/build_interface tga/texture_interface_dxtc.tga tga_tmp
+# TEMP ../../bin/tga2dds tga_tmp/texture_interface_dxtc.tga -o tga/texture_interface_dxtc.dds -a 5
+
+# Idle
+../../idle.bat
 
 

e06830802888e838b1f30b2b6d44a8fe0b63eb1a
Olivier Cado <cado@nevrax.com>
2003-07-09 17:20:00 +0200

    ADDED: Sub-message scheme

--- nel/include/nel/net/message.h
+++ nel/include/nel/net/message.h
@@ -1,7 +1,7 @@
 /** \file message.h
  * From memory serialization implementation of IStream with typed system (look at stream.h)
  *
- * $Id: message.h,v 1.34 2002/08/20 11:47:39 cado Exp $
+ * $Id: message.h,v 1.35 2003/07/09 15:17:08 cado Exp $
  */
 
 /* Copyright, 2000 Nevrax Ltd.
@@ -38,6 +38,8 @@
 
 namespace NLNET
 {
+
+extern const char *LockedSubMessageError;
 
 
 /**
@@ -77,25 +79,130 @@ public:
 	/// Sets the message type as a string and put it in the buffer if we are in writing mode
 	void setType (const std::string &name);
 
+	void changeType (const std::string &name);
+
 	/// Returns the size, in byte of the header that contains the type name of the message or the type number
 	uint32 getHeaderSize ();
 	
-	// The message was filled with an CMemStream, Now, we'll get the message type on this buffer
+	/** The message was filled with an CMemStream, Now, we'll get the message type on this buffer.
+	 * This method updates _LengthR with the actual size of the buffer (it calls resetLengthR()).
+	 */
 	void readType ();
+
+	/// Get the message name (input message only) and advance the current pos
+	std::string readTypeAtCurrentPos();
 
 	// Returns true if the message type was already set
 	bool typeIsSet () const;
 
-	// Transforms the message from input to output or from output to input
+	/** Returns the length (size) of the message, in bytes.
+	 * If isReading(), it is the number of bytes that can be read,
+	 * otherwise it is the number of bytes that have been written.
+	 */
+	virtual uint32	length() const
+	{
+		if ( isReading() )
+		{
+			return lengthR();
+		}
+		else
+		{
+			return lengthS();
+		}
+	}
+
+	/// Returns the "read" message size (number of bytes to read) (note: see comment about _LengthR)
+	uint32			lengthR() const
+	{
+		return _LengthR;
+	}
+
+	/**
+	 * Set an input message to look like, from a message callback's scope, as if it began at the current
+	 * pos and ended at the current pos + msgSize, and read the header and return the name of the sub message.
+	 *
+	 * This method provides a way to pass a big message containing a set of sub messages to their message
+	 * callback, without copying each sub message to a new message. If you need to perform some actions
+	 * that are not allowed with a locked message (see postconditions), use assignFromSubMessage():
+	 * the locked sub message in M1 can be copied to a new message M2 with 'M2.assignFromSubMessage( M1 )'.
+	 * 
+	 * Preconditions:
+	 * - The message is an input message (isReading())
+	 * - msgEndPos <= length()
+	 * - getPos() >= getHeaderSize()
+	 *
+	 * Postconditions:
+	 * - The sub message is ready to be read from
+	 * - length() returns the size of the sub message
+	 * - getName() return the name of the sub message
+	 * - Unless you call unlockSubMessage(), the following actions will assert or raise an exception:
+	 *   Serializing more than the sub message size, clear(), operator=() (from/to), invert().
+	 */
+	std::string			lockSubMessage( uint32 subMsgSize )
+	{
+		uint32 subMsgBeginPos = getPos();
+		uint32 subMsgEndPos = subMsgBeginPos + subMsgSize;
+		nlassertex( isReading() && (subMsgEndPos <= lengthR()), ("%s %u %u", isReading()?"R":"W", subMsgEndPos, lengthR()) );
+		std::string name = readTypeAtCurrentPos();
+		_SubMessagePosR = subMsgBeginPos;
+		_LengthR = subMsgEndPos;
+		return name;
+	}
+
+	/**
+	 * Exit from sub message locking, and skip the whole sub message.
+	 *
+	 * Preconditions:
+	 * - The message is an input message (isReading()) and has been locked using lockSubMessage()
+	 *
+	 * Postconditions:
+	 * - The current pos is the next byte after the sub message
+	 */
+	void			unlockSubMessage()
+	{
+		nlassert( isReading() && hasLockedSubMessage() );
+		nlassert( getPos() <= (sint32)_LengthR ); // locking the message ensures the user can't read more
+		uint32 subMsgEndPos = _LengthR;
+		resetSubMessageInternals();
+		seek( subMsgEndPos, IStream::begin );
+	}
+
+	/// Return true if a sub message has been locked
+	bool			hasLockedSubMessage() const
+	{
+		return (_SubMessagePosR != 0);
+	}
+
+	/**
+	 * Similar to operator=, but makes the current message contain *only* the locked sub message in msgin
+	 * or the whole msgin if it is not locked
+	 *
+	 * Preconditions:
+	 * - msgin is an input message (isReading())
+	 * - The current message is blank (new or reset with clear())
+	 *
+	 * Postconditions:
+	 * - If msgin has been locked using lockSubMessage(), the current message contains only the locked
+	 *   sub message in msgin, otherwise the current message is exactly msgin
+	 * - The current message is an input message, it is not locked
+	 */
+	void			assignFromSubMessage( const CMessage& msgin );
+
+	/**
+	 * Transforms the message from input to output or from output to input
+	 */
 	void invert()
 	{
+		nlassertex( (!isReading()) || (!hasLockedSubMessage()), ("Inverting %s", LockedSubMessageError) );
+
 		CMemStream::invert();
 
 		if ( isReading() )
 		{
 			// Write -> Read: skip the header 
 			_TypeSet = false;
-			readType();
+			if ( _Buffer.size() != 0 )
+				readType();
 		}
 		// For Read -> Write, please use clear()
 	}
@@ -103,7 +210,9 @@ public:
 	// Clear the message. With this function, you can reuse a message to create another message
 	void clear ();
 
-	/// Returns the type name in string if available. Be sure that the message have the name of the message type
+	/** Returns the type name in string if available. Be sure that the message have the name of the message type
+	 * In a callback driven by message name, getName() does not necessarily return the right name.
+	 */
 	std::string getName () const;
 	
 	/// Returns the type id of this message is available.
@@ -117,6 +226,12 @@ public:
 	/// Set default stream mode
 	static void	setDefaultStringMode( bool stringmode ) { _DefaultStringMode = stringmode; }
 
+	/// Return an input stream containing the stream beginning in the message at the specified pos
+	NLMISC::CMemStream	extractStreamFromPos( sint32 pos );
+
+	/// Encapsulate/decapsulate another message inside the current message
+	void	serialMessage( CMessage& msg );
+
 	bool TypeHasAnId;
 	bool TypeHasAName;
 
@@ -125,15 +240,30 @@ protected:
 	/// Utility method
 	void		init( const std::string &name, TStreamFormat streamformat );
 
+	/// Utility method
+	void		resetSubMessageInternals()
+	{
+		_SubMessagePosR = 0;
+		_LengthR = _Buffer.size();
+	}
+
 private:
-	bool								_TypeSet;
 	NLMISC::CStringIdArray				*_SIDA;
 	
 	std::string							_Name;
-	NLMISC::CStringIdArray::TStringId	_Id;
 	
+	// When sub message lock mode is enabled, beginning position of sub message to read (before header)
+	uint32								_SubMessagePosR;
+
+	// Length (can be smaller than _Buffer.size() if limitLength() is used) (updated in reading mode only)
+	uint32								_LengthR;
+
 	// Size of the header (that contains the name type or number type)
 	uint32								_HeaderSize;
+
+	NLMISC::CStringIdArray::TStringId	_Id;
+
+	bool								_TypeSet;
 
 	// Default stream format
 	static bool							_DefaultStringMode;

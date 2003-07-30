3328ee2991b97b56d3aa3fcdfd34b2e8e02824c7
Nicolas Vizerie <vizerie@nevrax.com>
2003-07-30 18:48:54 +0200

    #CHANGE: optFastFloor is in NLMISC now

--- nel/tools/3d/object_viewer/vegetable_apperance_page.cpp
+++ nel/tools/3d/object_viewer/vegetable_apperance_page.cpp
@@ -8,7 +8,7 @@
 #include "vegetable_noise_value_dlg.h"
 #include "vegetable_select_dlg.h"
 #include "vegetable_dlg.h"
-#include "3d/noise_value.h"
+#include "nel/misc/noise_value.h"
 #include "3d/vegetable.h"
 
 

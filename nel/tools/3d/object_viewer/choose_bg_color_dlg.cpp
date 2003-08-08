127a5babba4ec861215b5b755cfa7058bdab6dff
Nicolas Vizerie <vizerie@nevrax.com>
2003-08-08 18:57:39 +0200

    #CHANGE:  new macro removed

--- nel/tools/3d/object_viewer/choose_bg_color_dlg.cpp
+++ nel/tools/3d/object_viewer/choose_bg_color_dlg.cpp
@@ -6,11 +6,7 @@
 #include "choose_bg_color_dlg.h"
 #include "color_edit.h"
 
-#ifdef _DEBUG
-#define new DEBUG_NEW
-#undef THIS_FILE
-static char THIS_FILE[] = __FILE__;
-#endif
+
 
 /////////////////////////////////////////////////////////////////////////////
 // CChooseBGColorDlg dialog

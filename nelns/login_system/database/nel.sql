f955817c22eedeee36fca6500aaacc25ddbd9c9c
Vianney Lecroart <lecroart@nevrax.com>
2003-10-20 18:25:41 +0200

    CHANGED: new database format

--- nelns/login_system/database/nel.sql
+++ nelns/login_system/database/nel.sql
@@ -1,8 +1,8 @@
 # MySQL-Front Dump 2.4
 #
-# Host: localhost   Database: nel
+# Host: net2   Database: nel
 #--------------------------------------------------------
-# Server version 3.23.34
+# Server version 4.0.12-log
 
 USE nel;
 
@@ -11,10 +11,10 @@ USE nel;
 # Table structure for table 'permission'
 #
 
-DROP TABLE IF EXISTS permission;
 CREATE TABLE `permission` (
   `UId` int(10) unsigned NOT NULL default '0',
-  `ClientApplication` char(64) NOT NULL default 'snowballs'
+  `ClientApplication` char(64) NOT NULL default 'ryzom',
+  `ShardId` int(10) NOT NULL default '-1'
 ) TYPE=MyISAM;
 
 
@@ -23,14 +23,13 @@ CREATE TABLE `permission` (
 # Table structure for table 'shard'
 #
 
-DROP TABLE IF EXISTS shard;
 CREATE TABLE `shard` (
   `ShardId` int(10) NOT NULL auto_increment,
   `WsAddr` varchar(64) default NULL,
   `NbPlayers` int(10) unsigned default '0',
-  `Name` varchar(64) default NULL,
+  `Name` varchar(64) default 'unknown shard',
   `Online` tinyint(1) unsigned default '0',
-  `ClientApplication` varchar(64) NOT NULL default '',
+  `ClientApplication` varchar(64) default 'ryzom',
   `Version` varchar(64) default NULL,
   PRIMARY KEY  (`ShardId`)
 ) TYPE=MyISAM COMMENT='contains all shards informations for login system';
@@ -41,13 +40,13 @@ CREATE TABLE `shard` (
 # Table structure for table 'user'
 #
 
-DROP TABLE IF EXISTS user;
 CREATE TABLE `user` (
   `UId` int(10) NOT NULL auto_increment,
   `Login` varchar(64) NOT NULL default '',
   `Password` tinytext NOT NULL,
   `ShardId` int(10) NOT NULL default '-1',
   `State` enum('Offline','Online') NOT NULL default 'Offline',
+  `Privilege` varchar(255) default NULL,
   PRIMARY KEY  (`UId`)
 ) TYPE=MyISAM COMMENT='contains all users informations for login system';
 

1f1f676f47939f6d56f5cf53782da17c0e38d385
Vianney Lecroart <lecroart@nevrax.com>
2004-01-06 17:01:32 +0100

    ADDED: new launcher

---
+++ nelns/login_system/nel_launcher_windows_ext2/std_afx.cpp
@@ -0,0 +1,8 @@
+// stdafx.cpp : source file that includes just the standard includes
+//	nel_launcher.pch will be the pre-compiled header
+//	std_afx.obj will contain the pre-compiled type information
+
+#include "std_afx.h"
+
+
+

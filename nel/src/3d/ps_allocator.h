1f5af6dd97d8ca418fa8ff6efc31acb79d7631d1
Nicolas Vizerie <vizerie@nevrax.com>
2004-03-19 11:45:19 +0100

    #BUGFIX: template prb with VC7

--- nel/src/3d/ps_allocator.h
+++ nel/src/3d/ps_allocator.h
@@ -1,6 +1,6 @@
 /** \file ps_allocator.h
  *
- * $Id: ps_allocator.h,v 1.2 2004/03/08 11:21:18 vizerie Exp $
+ * $Id: ps_allocator.h,v 1.3 2004/03/19 10:45:19 vizerie Exp $
  */
 
 /* Copyright, 2000, 2001, 2002, 2003 Nevrax Ltd.
@@ -33,7 +33,7 @@
 namespace NL3D
 {
 
-#ifdef NL_OS_WINDOWS
+#if	defined(NL_OS_WINDOWS) && !defined(NL_COMP_VC7)
 	// fast mem alloc of particle systems only on windows for now
 	//
 	// NB : When using NL_MEMORY, we solve the redefinition of new errors as follow :	
@@ -45,7 +45,7 @@ namespace NL3D
 	// #if !defined (NL_USE_DEFAULT_MEMORY_MANAGER) && !defined (NL_NO_DEFINE_NEW)
 	//	   #define new NL_NEW
 	// #endif
-	#define PS_FAST_ALLOC
+	#define PS_FAST_ALLOC	
 #endif
 
 
@@ -152,7 +152,7 @@ namespace NL3D
 	private:
 		std::allocator<uint8> _Alloc;
 	};
-
+	
 
 	// allocation of objects of ps (to be used inside base class declaration, replaces operator new & delete)
 	#if !defined (NL_USE_DEFAULT_MEMORY_MANAGER) && !defined (NL_NO_DEFINE_NEW)

95b7a255ad7b1d72c13d79f8422662e0ccaf15d3
Vianney Lecroart <lecroart@nevrax.com>
2004-06-03 19:14:41 +0200

    ADDED: disable callback on ctor

--- nel/include/nel/misc/variable.h
+++ nel/include/nel/misc/variable.h
@@ -1,7 +1,7 @@
 /** \file variable.h
  * Management of runtime variable
  *
- * $Id: variable.h,v 1.10 2004/04/07 15:15:53 cado Exp $
+ * $Id: variable.h,v 1.11 2004/06/03 17:14:41 lecroart Exp $
  */
 
 /* Copyright, 2003 Nevrax Ltd.
@@ -223,7 +223,7 @@ public:
 	CVariable (const char *commandName, const char *commandHelp, const T &defaultValue, uint nbMeanValue = 0, bool useConfigFile = false, void (*cc)(IVariable &var)=NULL) :
 		IVariable (commandName, commandHelp, "[<value>|stat|mean|min|max]", useConfigFile, cc), _Mean(nbMeanValue), _First(true)
 	{
-		set (defaultValue);
+		set (defaultValue, false);
 	}
 
 	virtual void fromString (const std::string &val, bool human=false)
@@ -254,7 +254,7 @@ public:
 		return get ();
 	}
 
-	void set (const T &val)
+	void set (const T &val, bool callCallback = true)
 	{
 		_Value = val;
 		_Mean.addValue (_Value);
@@ -269,7 +269,7 @@ public:
 			if (_Value > _Max) _Max = _Value;
 			if (_Value < _Min) _Min = _Value;
 		}
-		if (ChangeCallback) ChangeCallback (*this);
+		if (ChangeCallback && callCallback) ChangeCallback (*this);
 	}
 	
 	const T &get () const

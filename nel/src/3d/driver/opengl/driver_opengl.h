37e89f5a5ed7c522b21cb36385e12f28c4642abf
Vianney Lecroart <lecroart@nevrax.com>
2004-08-23 13:59:45 +0200

    FIXED: move the h auto define

--- nel/src/3d/driver/opengl/driver_opengl.h
+++ nel/src/3d/driver/opengl/driver_opengl.h
@@ -1,7 +1,7 @@
 /** \file driver_opengl.h
  * OpenGL driver implementation
  *
- * $Id: driver_opengl.h,v 1.178 2004/08/13 15:31:54 vizerie Exp $
+ * $Id: driver_opengl.h,v 1.179 2004/08/23 11:59:45 lecroart Exp $
  */
 
 /* Copyright, 2000 Nevrax Ltd.
@@ -29,6 +29,13 @@
 
 #include "nel/misc/types_nl.h"
 
+//#define NL_PROFILE_DRIVER_OGL
+#ifdef NL_PROFILE_DRIVER_OGL
+#define H_AUTO_OGL(label) H_AUTO(label)
+#else
+#define H_AUTO_OGL(label)
+#endif
+
 #ifdef NL_OS_WINDOWS
 
 #define WIN32_LEAN_AND_MEAN
@@ -39,14 +46,6 @@
 #  ifdef max
 #    undef max
 #  endif
-
-//#define NL_PROFILE_DRIVER_OGL
-#ifdef NL_PROFILE_DRIVER_OGL
-#define H_AUTO_OGL(label) H_AUTO(label)
-#else
-#define H_AUTO_OGL(label)
-#endif
-
 
 #else // NL_OS_UNIX
 

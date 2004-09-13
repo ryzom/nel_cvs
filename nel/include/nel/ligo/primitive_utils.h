a9fc6d9688b85ac8b3bbcb7679cafdcc0c660377
Boris Boucher <boucher@nevrax.com>
2004-09-13 20:39:46 +0200

    BUGFIX : const problem

--- nel/include/nel/ligo/primitive_utils.h
+++ nel/include/nel/ligo/primitive_utils.h
@@ -28,7 +28,7 @@
  *
  *	Boris.
  *
- * $Id: primitive_utils.h,v 1.6.4.1 2004/09/13 15:56:40 boucher Exp $
+ * $Id: primitive_utils.h,v 1.6.4.2 2004/09/13 18:39:46 boucher Exp $
  */
 
 /* Copyright, 2000-2002 Nevrax Ltd.
@@ -343,13 +343,15 @@ inline bool saveXmlPrimitiveFile(CPrimitives &primDoc, const std::string &fileNa
  *	you must use filterPrimitiveChilds to do this.
  */
 template <class Pred>
-IPrimitive *getPrimitiveChild(const IPrimitive *parent, Pred &predicate)
+IPrimitive *getPrimitiveChild(IPrimitive *parent, Pred &predicate)
 {
 	for (uint i=0; i<parent->getNumChildren(); ++i)
 	{
 		IPrimitive *child;
 		if (parent->getChild(child, i) && predicate(child))
+		{
 			return child;
+		}
 	}
 
 	return NULL;

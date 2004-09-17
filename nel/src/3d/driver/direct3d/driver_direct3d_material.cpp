a82926d150157fe2901753b977a00b8cc2ceb9e4
Nicolas Vizerie <vizerie@nevrax.com>
2004-09-17 17:09:19 +0200

    #BUGFIX: performances counters were not at the right place

--- nel/src/3d/driver/direct3d/driver_direct3d_material.cpp
+++ nel/src/3d/driver/direct3d/driver_direct3d_material.cpp
@@ -1,7 +1,7 @@
 /** \file driver_direct3d_material.cpp
  * Direct 3d driver implementation
  *
- * $Id: driver_direct3d_material.cpp,v 1.15 2004/09/02 16:56:59 vizerie Exp $
+ * $Id: driver_direct3d_material.cpp,v 1.16 2004/09/17 15:09:19 vizerie Exp $
  *
  * \todo manage better the init/release system (if a throw occurs in the init, we must release correctly the driver)
  */
@@ -303,8 +303,8 @@ bool CDriverD3D::setupMaterial(CMaterial &mat)
 	// Now we can get the supported shader from the cache.
 	CMaterial::TShader matShader = mat.getShader();
 
-	H_AUTO_D3D(CDriverD3D_setupMaterial_light)
 	{			
+		H_AUTO_D3D(CDriverD3D_setupMaterial_light)
 		// if the shader has changed since last time
 		if(matShader != _CurrentMaterialSupportedShader)
 		{
@@ -339,8 +339,8 @@ bool CDriverD3D::setupMaterial(CMaterial &mat)
 	}
 	
 
-	H_AUTO_D3D(CDriverD3D_setupMaterial_touchupdate)
 	{
+		H_AUTO_D3D(CDriverD3D_setupMaterial_touchupdate)
 		// Something to setup ?
 		if (touched)
 		{					
@@ -543,8 +543,8 @@ bool CDriverD3D::setupMaterial(CMaterial &mat)
 	}
 
 
-	H_AUTO_D3D(CDriverD3D_setupMaterial_bindTexture)
 	{
+		H_AUTO_D3D(CDriverD3D_setupMaterial_bindTexture)
 		// 2. Setup / Bind Textures.
 		//==========================
 		// Must setup textures each frame. (need to test if touched).
@@ -568,8 +568,8 @@ bool CDriverD3D::setupMaterial(CMaterial &mat)
 	// Do not do it for Lightmap and per pixel lighting , because done in multipass in a very special fashion.
 	// This avoid the useless multiple change of texture states per lightmapped object.
 	// Don't do it also for Specular because the EnvFunction and the TexGen may be special.
-	H_AUTO_D3D(CDriverD3D_setupMaterial_normalShaderActivateTextures)
 	{
+		H_AUTO_D3D(CDriverD3D_setupMaterial_normalShaderActivateTextures)
 		if(matShader == CMaterial::Normal)
 		{
 			uint stage;
@@ -816,8 +816,9 @@ bool CDriverD3D::setupMaterial(CMaterial &mat)
 				activeShader (NULL);
 
 				/* If unlighted trick is needed, set the shader later */
-				if (!pShader->NeedsConstantForDiffuse)
-					setPixelShader (pShader->PixelShader);
+				//if (!pShader->NeedsConstantForDiffuse)
+				//	setPixelShader (pShader->PixelShader);
+				setPixelShader (!pShader->NeedsConstantForDiffuse ? pShader->PixelShader : NULL);
 			}
 			break;
 		case CMaterial::LightMap:

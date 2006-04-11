220527e384fdaeeb9677d65a2957bf96a6a05462
Patrick Bernard <bernard@nevrax.com>
2006-04-11 18:27:52 +0200

    FIXED: agreement is in the right place now

--- web/ryzom_com/RyzomSkin/skins/ryzomskin_script/agree_script.py
+++ web/ryzom_com/RyzomSkin/skins/ryzomskin_script/agree_script.py
@@ -8,13 +8,13 @@
 ##title=
 ##
 
-text1en="By clicking the \"OK\" button, you hereby agree to grant permission to copy, distribute and/or modify this document under the terms of the GNU Free Documentation License, Version 1.2 or any later version published by the Free Software Foundation; with no Invariant Sections, no Front-Cover Texts, and no Back-Cover Texts.  A copy of the license can be found <a href=\"/support/policies/gnu-free-documentation-license\">here</a> and the original version of the license can be found <a href=\"http://www.gnu.org/copyleft/fdl.html\">here</a>.<p />"
-text1fr=text1en+'fr'
-text1de=text1en+'de'
+text1en="By clicking the \"OK\" button, you hereby agree to grant permission to copy, distribute and/or modify this document under the terms of the GNU Free Documentation License, Version 1.2 or any later version published by the Free Software Foundation; with no Invariant Sections, no Front-Cover Texts, and no Back-Cover Texts.  A copy of the license can be found <a href=\"/support/policies/gnu-free-documentation-license\">here</a> and the original version of the license can be found <a href=\"http://www.gnu.org/copyleft/fdl.html\">here</a>.<br />"
+text1fr=text1en
+text1de=text1en
 
-text2en="By clicking the \"OK\" button, you hereby agree with the terms of the <a href=\"/support/policies/fan-art-submissions-license-agreement\">Fan Art Submissions License Agreement</a>. Otherwise, click \"Cancel\".<p />"
-text2fr=text2en+'fr'
-text2de=text2en+'de'
+text2en="By clicking the \"OK\" button, you hereby agree with the terms of the <a href=\"/support/policies/fan-art-submissions-license-agreement\">Fan Art Submissions License Agreement</a>. Otherwise, click \"Cancel\".<br />"
+text2fr=text2en
+text2de=text2en
 
 if type in ['ATDocument','ATNewsItem','ATEvent','WeblogEntry']:
   if lang=='fr':

aa3769068de4b42d15d1275a8957e4bd1ea4e6d8
Patrick Bernard <bernard@nevrax.com>
2006-04-26 14:17:10 +0200

    CHANGED: php script & external_** are displace to respectively /ryzom/php & /ryzom/skin/ryzom

---
+++ web/ryzom_com/Ryzom/php/header.php
@@ -0,0 +1,9 @@
+<?php include "http://localhost:9673/ryzom/external_header";?>
+<center>
+<div id="visual-portal-wrapper" style="width=1111px;background-color:#181818;">
+    <div id="portal-columns">
+      <?php include "http://localhost:9673/ryzom/external_left";?>
+      <?php include "http://localhost:9673/ryzom/external_right";?>
+      <div id="portal-column-content">
+        <div id="content">
+        <div class="documentContent" id="region-content">

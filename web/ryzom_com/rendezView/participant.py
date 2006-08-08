fc20ab8a5cd5344d513634fe5b53bafb4ea0b232
Patrick Bernard <bernard@nevrax.com>
2006-08-08 14:58:49 +0200

    CHANGED:add methode

--- web/ryzom_com/rendezView/participant.py
+++ web/ryzom_com/rendezView/participant.py
@@ -19,9 +19,10 @@ participantSchema=BaseSchema.copy()+ Schema((
 	TextField('login',
 		widget=TextAreaWidget(visible={'edit':'hidden', 'view':'hidden'},)		
 	),
-#	IntegerField('seat',
-#		widget=IntegerWidget(default=1)		
-#	),
+	IntegerField('seat',
+		default=1,
+		widget=IntegerWidget()		
+	),
 ),)
 participantSchema['id'].widget.visible = { 'view': 'hidden', 'edit': 'hidden',}
 participantSchema['title'].widget.visible = { 'view': 'hidden', 'edit': 'hidden',}
@@ -35,28 +36,28 @@ class participant(BaseContent):
 	meta_type = 'participant'
 	global_allow = 0
 
-	def setTitle(self):
-		value = self.getLogin()+'_'+self.getFirstName()+'_'+self.getLastName
-		self.getField('title').set(self, value, **kwargs)
+#	def setTitle(self):
+#		value = self.getLogin()+'_'+self.getFullName()
+#		self.getField('title').set(self, value, **kwargs)
 
-	def setFullName(self):
-		mtool = getToolByName(self, 'portal_membership')
-		member = mtool.getAuthenticatedMember()
-		value = member.getProperty('fullname')		
-		self.getField('fullName').set(self, value, **kwargs)
+#	def setFullName(self):
+#		mtool = getToolByName(self, 'portal_membership')
+#		member = mtool.getAuthenticatedMember()
+#		value = member.getProperty('fullname')
+#		self.getField('fullName').set(self, value, **kwargs)
 
-	def setEmail(self):
-		mtool = getToolByName(self, 'portal_membership')
-		member = mtool.getAuthenticatedMember()
-		value = member.getProperty('email')	
-		self.getField('email').set(self, value, **kwargs)
+#	def setEmail(self):
+#		mtool = getToolByName(self, 'portal_membership')
+#		member = mtool.getAuthenticatedMember()
+#		value = member.getProperty('email')
+#		self.getField('email').set(self, value, **kwargs)
 
-	def setLogin(self):
-		mtool = getToolByName(self, 'portal_membership')
-		member = mtool.getAuthenticatedMember()
-		value = mtool.getUserName()
-		self.getField('login').set(self, value, **kwargs)
-
+#	def setLogin(self):
+#		mtool = getToolByName(self, 'portal_membership')
+#		member = mtool.getAuthenticatedMember()
+#		value = member.getUserName()
+#		self.getField('login').set(self, value, **kwargs)
+	
 
 registerType(participant,PROJECTNAME)								
 								
--- web/ryzom_com/rendezView/rendezView.py
+++ web/ryzom_com/rendezView/rendezView.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+from Products.CMFCore.utils import getToolByName
 from Products.CMFCore import CMFCorePermissions
+from AccessControl import ClassSecurityInfo
 try:
     from Products.LinguaPlone.public import *
 except ImportError: 
@@ -28,9 +30,10 @@ rendezViewSchema=BaseFolderSchema.copy()+ Schema((
 	IntegerField('nbSeat',
 		widget=IntegerWidget(description="Nombre de place disponible",)
 	),
-#	IntergerField('maxSeat',
-#		widget=IntegerWidget(description="Nombre de place rÃ©servable au maximum",default=1,)
-#	),
+	IntegerField('maxSeat',
+		default=1,
+		widget=IntegerWidget(description="Nombre de place rÃ©servable au maximum par personne",)
+	),
 #	LinesField('participant',		
 #		widget=LinesWidget(),
 #		visible={'edit':'hidden', 'view':'visible'},
@@ -43,7 +46,7 @@ class rendezView(BaseFolder):
 
 	"""Add an rendezView event"""
 	schema = rendezViewSchema
-
+	security = ClassSecurityInfo()
 	archetype_name = "rendezView"
 	meta_type = 'rendezView'
 	default_view  = 'rendezView_view'
@@ -65,12 +68,65 @@ class rendezView(BaseFolder):
 	)
 
 	def setTitle(self, value, **kwargs):
+		"""remplace l'id par un identificateur plus conviviale"""
 		self.getField('title').set(self, value, **kwargs)
 		if value:
 			try:
 				self.setId(re.sub('[^A-Za-z0-9_-]', '', re.sub(' ', '-', value)).lower())
 			except:
 				pass #try to do better than this
+
+	#test si un membres peut s'inscrire, (s'il reste de la place et s'il n'est pas dÃ©ja inscrit)
+	#dÃ©ja inscrit ? on testera dans la vue
+	def isAvailable(self):
+		"""test si des places sont disponible"""
+		#on recupere le membre
+		#mtool = getToolByName(self, 'portal_membership')
+		#member = mtool.getAuthenticatedMember()
+		#id = member.getUserName()+'_'+member.getProperty('fullname')
+		#recuperer la liste des participants de l'event, et le nombre de place reserver
+		s = 0
+		tab = self.getParticipant()		
+		for i in tab:
+			o = i.getObject()
+			s += o.getSeat()
+		return s < self.getNbSeat()
+
+
+	def nbParticipant(self):
+		"""renvoie le nombre d'inscriptions"""		
+		tab = self.getParticipant()
+		return len(tab)
+			
+		
+	def getParticipant(self):
+		"""renvoie la listes des participants"""
+		path = '/'.join(self.getPhysicalPath())
+		results = self.portal_catalog(meta_type=['participant',],path={'query':path, 'level': 0},)
+		return results
+
+	def addParticipant(self,seat=1):
+		"""ajoute une inscription"""
+		path = '/'.join(self.getPhysicalPath())
+		mtool = getToolByName(self, 'portal_membership')
+		member = mtool.getAuthenticatedMember()
+		fullname = member.getProperty('fullname')
+		login = member.getUserName()
+		email = member.getProperty('email')
+		seat = seat
+
+		newid = self.getId()+'_'+login+'_'+fullname
+		title = login+'_'+fullname		
+		self.invokeFactory(type_name='participant', id=newid, title=title, description='', fullName=fullname, email=email, seat=seat)
+		new_obj = getattr(self, newid)
+		new_obj.setFullName(fullname)
+		new_obj.setLogin(login)
+		new_obj.setEmail(email)
+		new_obj.setSeat(seat)
+
+		return "[inscription success]"
+	
+		
 
 
 registerType(rendezView,PROJECTNAME)								

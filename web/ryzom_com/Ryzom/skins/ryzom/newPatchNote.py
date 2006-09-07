a5feb2b1afc9759a60377fb9472dc2e64284b88f
Patrick Bernard <bernard@nevrax.com>
2006-09-07 09:47:06 +0200

    ADDED: title for translated patch

--- web/ryzom_com/Ryzom/skins/ryzom/newPatchNote.py
+++ web/ryzom_com/Ryzom/skins/ryzom/newPatchNote.py
@@ -17,19 +17,19 @@ if context.portal_catalog(id='patch-'+patch_en[0][7], meta_type='UrlPatchNote', 
 
 context.invokeFactory(id='patch-'+patch_en[0][7], type_name='UrlPatchNote', title=patch_en[0][7], description='', text=patch_en[0][4])
 new_obj = getattr(context, 'patch-'+patch_en[0][7])
-new_obj.setTitle(patch_en[0][7])
+new_obj.setTitle('patch '+patch_en[0][7])
 new_obj.setText(patch_en[0][4],mimetype='text/html')
 
 patch_fr = context.patchNoteQuery(lang='fr')
 new_obj.createTranslation(set_language="fr", current="en")
 new_obj_fr = new_obj.getTranslation("fr")
-new_obj_fr.setTitle(patch_fr[0][7])
+new_obj_fr.setTitle('patch '+patch_fr[0][7])
 new_obj_fr.setText(patch_fr[0][4],mimetype='text/html')
 
 patch_de = context.patchNoteQuery(lang='de')
 new_obj.createTranslation(set_language="de", current="en")
 new_obj_de = new_obj.getTranslation("de")
-new_obj_de.setTitle(patch_de[0][7])
+new_obj_de.setTitle('patch '+patch_de[0][7])
 new_obj_de.setText(patch_de[0][4],mimetype='text/html')
 
 request = container.REQUEST

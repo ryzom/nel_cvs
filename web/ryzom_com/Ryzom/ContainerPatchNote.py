40433fd1e819d9f95203f655fc2ad792af7ccab0
Patrick Bernard <bernard@nevrax.com>
2006-09-11 13:10:27 +0200

    FIXED: id is generated by archetype/plone now

--- web/ryzom_com/Ryzom/ContainerPatchNote.py
+++ web/ryzom_com/Ryzom/ContainerPatchNote.py
@@ -15,6 +15,7 @@ class ContainerPatchNote(BaseFolder):
 	schema = ContainerPatchNoteSchema
 	meta_type = portal_type = 'ContainerPatchNote'
 	allowed_content_types = ('UrlPatchNote',)
+	_at_rename_after_creation = True
 
 	actions = (
 		{ 'id': 'view',
@@ -24,14 +25,14 @@ class ContainerPatchNote(BaseFolder):
 		},
 	)
 
-	def setTitle(self, value, **kwargs):
-		if not value and self.id:
-			value = self.id
-		else:			
-			try:
-				self.setId(re.sub('[^A-Za-z0-9_-]', '', re.sub(' ', '-', value)).lower())
-			except:
-				pass
-		self.getField('title').set(self, value, **kwargs)
+#	def setTitle(self, value, **kwargs):
+#		if not value and self.id:
+#			value = self.id
+#		else:			
+#			try:
+#				self.setId(re.sub('[^A-Za-z0-9_-]', '', re.sub(' ', '-', value)).lower())
+#			except:
+#				pass
+#		self.getField('title').set(self, value, **kwargs)
 
 registerType(ContainerPatchNote, PROJECTNAME)
--- web/ryzom_com/Ryzom/Redirect.py
+++ web/ryzom_com/Ryzom/Redirect.py
@@ -22,6 +22,8 @@ class Redirect(BaseContent):
 
 	schema = RedirectSchema
 	meta_type = portal_type = 'Redirect'
+	_at_rename_after_creation = True
+
 	actions = (
 		{ 'id': 'view',
 		'name': 'View',
@@ -29,15 +31,16 @@ class Redirect(BaseContent):
 		'permissions': (CMFCorePermissions.View,)
 		},
 	)
-	def setTitle(self, value, **kwargs):
-		if not value and self.id:
-			value = self.id
-		else:			
-			try:
-				self.setId(re.sub('[^A-Za-z0-9_-]', '', re.sub(' ', '-', value)).lower())
-			except:
-				pass
-		self.getField('title').set(self, value, **kwargs)			
+
+#	def setTitle(self, value, **kwargs):
+#		if not value and self.id:
+#			value = self.id
+#		else:			
+#			try:
+#				self.setId(re.sub('[^A-Za-z0-9_-]', '', re.sub(' ', '-', value)).lower())
+#			except:
+#				pass
+#		self.getField('title').set(self, value, **kwargs)			
 
 
 registerType(Redirect,PROJECTNAME)

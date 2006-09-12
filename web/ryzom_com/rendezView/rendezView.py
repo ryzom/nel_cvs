2ddd7f9ffa49ff38a7545dcb560830692a59774f
Patrick Bernard <bernard@nevrax.com>
2006-09-12 17:01:19 +0200

    FIXED: id is generated by archetype/plone now

--- web/ryzom_com/rendezView/rendezView.py
+++ web/ryzom_com/rendezView/rendezView.py
@@ -131,6 +131,8 @@ class rendezView(BaseFolder):
 	default_view  = 'rendezView_view'
 	immediate_view = 'rendezView_view'
 	allowed_content_types = ('participant',)
+	_at_rename_after_creation = True
+
 	actions = (
 		{
 		'id': 'view',
@@ -152,16 +154,16 @@ class rendezView(BaseFolder):
 #		},
 	)
 
-
-	def setTitle(self, value, **kwargs):
-		if not value and self.id:
-			value = self.id
-		else:			
-			try:
-				self.setId(re.sub('[^A-Za-z0-9_-]', '', re.sub(' ', '-', value)).lower())
-			except:
-				pass
-		self.getField('title').set(self, value, **kwargs)
+# 	replace fonction by """_at_rename_after_creation = True"""
+#	def setTitle(self, value, **kwargs):
+#		if not value and self.id:
+#			value = self.id
+#		else:			
+#			try:
+#				self.setId(re.sub('[^A-Za-z0-9_-]', '', re.sub(' ', '-', value)).lower())
+#			except:
+#				pass
+#		self.getField('title').set(self, value, **kwargs)
 
 
 	def isAvailable(self):

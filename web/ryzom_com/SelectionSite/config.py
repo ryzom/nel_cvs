c5a8c2fb3e29ef138cabf2ebb175ba6cad70f037
cvs-fast-export <cvs-fast-export@ryzom.dev>
2006-09-14 18:56:08 +0200

    Synthetic commit for incomplete tag split_for_branch_community_patches

--- web/ryzom_com/SelectionSite/config.py
+++
@@ -1,9 +0,0 @@
-from Products.CMFCore.CMFCorePermissions import AddPortalContent
-from Products.Archetypes.public import DisplayList
-
-ADD_CONTENT_PERMISSION = AddPortalContent
-PROJECTNAME = "SelectionSite"
-SKINS_DIR = 'skins'
-
-GLOBALS = globals()
-

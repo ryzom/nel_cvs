66c18ee74ddb1cf8c78738a9a24803f47b2bc8e1
Patrick Bernard <bernard@nevrax.com>
2006-09-20 12:05:10 +0200

    FIXED: automated releasenote

--- web/ryzom_com/Ryzom/skins/ryzom/newPatchNote.py
+++ web/ryzom_com/Ryzom/skins/ryzom/newPatchNote.py
@@ -9,62 +9,30 @@
 ##
 #on recupere les donnees de la base
 
-lang = "en"
 path = '/'.join(context.getPhysicalPath())
-patch_en = context.patchNoteQuery(lang='en')
 
-#detect of existing last  patch-note
-if context.portal_catalog(id='patch-'+patch_en[0][7], meta_type='UrlPatchNote', path={'query':path, 'level': 0}):
-   return "already exist"
-
-#detect of serverside patch
-serverside=''
-if 'server' in patch_en[0][9]:
-	serverside='b'
-
-patchId = 'patch-'+patch_en[0][7]+serverside
-patchTitle = 'patch '+patch_en[0][7]+serverside
-patchText = patch_en[0][4]
-
-if patchText == '':
-   patchText = patch_fr[0][3]
-
-context.invokeFactory(id=patchId, type_name='UrlPatchNote', title=patchTitle, description='', text=patchText)
-new_obj = getattr(context, patchId)
-new_obj.setTitle(patchTitle)
-new_obj.setText(patchText,mimetype='text/html')
-
+###create canonical object in english
+patch = context.getReleaseNote('en', path)
+context.invokeFactory(id=patch[0], type_name='UrlPatchNote', title=patch[1], description='', text=patch[2])
+new_obj = getattr(context, patch[0])
+new_obj.setTitle(patch[1])
+new_obj.setText(patch[2],mimetype='text/html')
 
 
 ###french translation
-patch_fr = context.patchNoteQuery(lang='fr')
-
-patchId = 'patch-'+patch_fr[0][7]+serverside
-patchTitle = 'patch '+patch_fr[0][7]+serverside
-patchText = patch_fr[0][4]
-if patchText == '':
-   patchText = patch_fr[0][3]
-
+patch = context.getReleaseNote('fr', path)
 new_obj.createTranslation(set_language="fr", current="en")
 new_obj_fr = new_obj.getTranslation("fr")
-new_obj_fr.setTitle(patchTitle)
-new_obj_fr.setText(patchText,mimetype='text/html')
+new_obj_fr.setTitle(patch[1])
+new_obj_fr.setText(patch[2],mimetype='text/html')
+
 
 ###deutch translation
-patch_de = context.patchNoteQuery(lang='de')
-
-patchId = 'patch-'+patch_de[0][7]+serverside
-patchTitle = 'patch '+patch_de[0][7]+serverside
-patchText = patch_de[0][4]
-if patchText == '':
-   patchText = patch_fr[0][3]
-
+patch = context.getReleaseNote('de', path)
 new_obj.createTranslation(set_language="de", current="en")
 new_obj_de = new_obj.getTranslation("de")
-new_obj_fr.setTitle(patchTitle)
-new_obj_fr.setText(patchText,mimetype='text/html')
+new_obj_de.setTitle(patch[1])
+new_obj_de.setText(patch[2],mimetype='text/html')
 
-
-request = container.REQUEST
-RESPONSE =  request.RESPONSE
-RESPONSE.redirect(context.absolute_url())
+#go to the patchnote view
+container.REQUEST.RESPONSE.redirect(context.absolute_url())

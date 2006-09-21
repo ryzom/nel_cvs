606504e412e02fd3e3b5a02e6e48484a351d97d0
Olivier Cado <cado@nevrax.com>
2006-09-21 21:34:29 +0200

    CHANGED: Updated VC8 files (from Andi 'Debug' Ireland)

--- web/ryzom_com/PloneSubscription/doc/screenshot.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-xwd -nobdrs -out ~/zopes/clarins/Products/PloneSubscription/doc/output
-
--- web/ryzom_com/Ryzom/php/footer.php
+++
@@ -1,7 +0,0 @@
-        </div>
-        </div>      
-      </div>
-    </div>
-<?php include "http://localhost:9673/ryzom/external_footer";?>
-</div>
-</center>

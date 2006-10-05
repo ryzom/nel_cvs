05637a192e2fea1ab8e2f745f477a9bb7afe783f
Patrick Bernard <bernard@nevrax.com>
2006-10-05 10:01:30 +0200

    *** empty log message ***

--- web/ryzom_com/RingRanking/Extensions/MaintenanceToolbox.py
+++ web/ryzom_com/RingRanking/Extensions/MaintenanceToolbox.py
@@ -1,9 +1,9 @@
 def runUpdateRingRanking(self):
 	"""Maintenance Task for RingRanking"""
 	results = self.portal_catalog(meta_type='RingRanking')
-	report = []
+	report = ''
 	for brain in results:
 		obj = brain.getObject()
 		res = obj.updateAllRank()
-		report.append(str(obj.getId()) +'--'+ str(res))
+		report+=str(obj.getId()) +'--'+ str(res)+'\n'
 	return report

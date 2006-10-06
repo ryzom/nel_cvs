a9e279adbc666270016f7fed4dce0d8d6c1b20ed
Alexandre Stanislawski <stanislawski@nevrax.com>
2006-10-06 14:55:39 +0200

    *** empty log message ***

--- web/ryzom_com/RingRanking/skins/ringranking/ajax.js
+++ web/ryzom_com/RingRanking/skins/ringranking/ajax.js
@@ -1,11 +1,23 @@
-/*Script permettant d'ajaxifier le produit Aniki 'a tracking manager'*/
+/* Scripts permettant d'ajaxifier le produit Aniki 'a tracking manager' 
+   ici adapté pour les besoins de Ring Ranking
+   
+   TODO: wrapper toutes ces fonctions dans un seul et meme objet de 
+   maniere a pouvoir mettre facilement des fonctions de callback 
+   personalisés => cherche un moyen de ne pas passer pas des fonctions 
+   anonymes pour la fonction de callback*/
 
+
+/* Non liée à AJAX, cette fonction remplace le contenu d'un element dont
+   l'identifiant est idDom par newContent */
 function changeContentOf(idDom, newContent){
 	element = document.getElementById(idDom);
-	/*element.style = "";*/
 	element.innerHTML = newContent;
 }
 
+/* Non liée à AJAX, cette fonction permet de caché ou decaché un 
+   élément dont l'id est idElement suivant si il est, respectivement, 
+   décaché ou caché
+   Retourne faux si l'element n'existe pas*/
 function hideOrNot(idElement){
 	if(!idElement) return false;
 	
@@ -19,31 +31,35 @@ function hideOrNot(idElement){
 	}
 }
 
+/* Non liée à AJAX, cette simple fonction cache un element dont l'id 
+   est idElement
+   Retourne faux si l'élément n'existe pas. */
 function hide(idElement){
 	if(!idElement) return false;
 	var toHide = document.getElementById(idElement);
 	toHide.className='hidden';
 }
 
+/* Non liée à AJAX, cette simple fonction décache un element dont l'id 
+   est idElement
+   Retourne faux si l'élément n'existe pas. */
 function unHide(idElement){
 	if(!idElement) return false;
 	var toHide = document.getElementById(idElement);
 	toHide.className='notHidden';
 }
 
-function changeContentURL(idDom){
-	if (xmlhttp.readyState == 4){
-		if (xmlhttp.status == 200){
-			changeContentOf(idDom, xmlhttp.responseText);
-		}
-	}
-}
-
+/* Essaie de clarification... 
+   ne marche pas actuellement*/
 function myXmlHttp(id, xmlHttp){
 	this.xmlhttp = xmlHttp;
 	this.func = new Function('if (this.xmlhttp.readyState == 4){ if (this.xmlhttp.status == 200){ changeContentOf('+id+', this.xmlhttp.responseText);} }');
 }
 
+
+/* Fonction permettant de recupérer un objet XMLhttpRequest quelquesoit 
+   le navigateur.
+*/
 function getHTTPObject(){
 	var xmlhttp = false;
 	
@@ -77,11 +93,13 @@ function getHTTPObject(){
 		}
 	}
 	
-
 	return xmlhttp;
 }
 
-/*Layout d'une fonction de callback classique*/
+/* Layout d'une fonction de callback classique
+   Cette fonction est appelée a chaque changement d'etat
+   Quand l'état de la requète est 4 (contenu reçu) alors elle affiche
+   le contenu dans une une boite de dialogue "alert" */
 function example(){
 	/*Verifie si l'etat est bien 4 (fini)*/
 	if (xmlhttp.readyState == 4){
@@ -96,15 +114,18 @@ function example(){
 	}
 }
 
+/* Fonction qui envoie une requete, url, et qui remplace le contenu de 
+   idDom par la reponse a la requete*/
 function sendRequestAndChangeContent(idDom, url){
 	var httpTruc = getHTTPObject();
-	if (httpTruc){
+	if (httpTruc){  //Si notre objet est bien créé alors on continue
 		unHide(idDom);
-		httpTruc.onreadystatechange = function(){
-			changeContentOf(idDom, "chargement...");
-			if (httpTruc.readyState == 4){ 
+		httpTruc.onreadystatechange = 
+		function(){ // C'est mal les fonctions anonymes!
+			changeContentOf(idDom, "chargement..."); //Par défaut, on affiche un message indiquant le chargement
+			if (httpTruc.readyState == 4){ // cf fonction canevas ci dessus
 				if (httpTruc.status == 200){
-					changeContentOf(idDom, httpTruc.responseText);
+					changeContentOf(idDom, httpTruc.responseText); //On change le contenu de idDom
 				} 
 				else {
 					changeContentOf(idDom, "erreur");
@@ -112,14 +133,18 @@ function sendRequestAndChangeContent(idDom, url){
 			}
 		}
 		try {
-			httpTruc.open("GET", url, true);
+			httpTruc.open("GET", url, true); //On envoie la requete
 		}
 		catch(e){
-			changeContentOf(idDom, 'erreur de chargement !!!');
+			changeContentOf(idDom, 'erreur de chargement !!!'); 
 			return false;
 		}
 		
 		
 		httpTruc.send(null);
+	}
+	else {
+		alert('Javascript error: your browser is too old or too dumb'+
+				' or both...');
 	}
 }

04b98bcdc53b90eb761cb52e64db2b0b9e1061cc
cvs-fast-export <cvs-fast-export@ryzom.dev>
2006-10-31 17:10:51 +0100

    Synthetic commit for incomplete tag split_for_branch_asian_ime

--- web/ryzom_com/RingRanking/Extensions/MaintenanceToolbox.py
+++
@@ -1,9 +0,0 @@
-def runUpdateRingRanking(self):
-	"""Maintenance Task for RingRanking"""
-	results = self.portal_catalog(meta_type='RingRanking')
-	report = ''
-	for brain in results:
-		obj = brain.getObject()
-		res = obj.updateAllRank()
-		report+=str(obj.getId()) +'--'+ str(res)+'\n'
-	return report

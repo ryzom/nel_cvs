d10e52b42a43173df638b9eac485eb27b213a5be
Patrick Bernard <bernard@nevrax.com>
2007-01-04 17:17:45 +0100

    ADDED: download_file script

---
+++ web/ryzom_com/Ryzom/skins/ryzom/download_file.py
@@ -0,0 +1,18 @@
+## Script (Python) "testpy"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=file
+##title=
+##
+date = context.Date()[:10]
+try:
+   result = context.checkDate(date=date,file=file)   
+   if len(result) == 0:
+      context.insertDate(date=date,file=file)
+   context.incremente_dl(date=date,file=file)
+except:
+   pass
+container.REQUEST.RESPONSE.redirect(file)
